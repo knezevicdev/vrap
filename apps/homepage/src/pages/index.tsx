ec814a9f3bfdf141ba679b903f542cc4a99a58fa
Kevin Parrott <kevin.parrott@contractor.vroom.com>
2020-05-13 10:57:21 -0400

    add getInitialProps for server side render

--- apps/homepage/src/pages/index.tsx
+++ apps/homepage/src/pages/index.tsx
@@ -4,10 +4,12 @@ import React from 'react';
 import Home from 'src/modules/home';
 import Page from 'src/Page';
 
-const HomePage: NextPage = () => {
-  const title = 'Vroom: Buy, Sell or Trade-In Used Vehicles Online';
-  const description =
-    'Buy, sell or trade-in a certified used car online from anywhere in the USA. We offer no-haggle car buying, top quality cars, full warranties & home shipping.';
+interface Props {
+  title: string;
+  description: string;
+}
+
+const HomePage: NextPage<Props> = ({ title, description }) => {
   const head = (
     <>
       <title>{title}</title>
@@ -20,6 +22,16 @@ const HomePage: NextPage = () => {
       <Home />
     </Page>
   );
+};
+
+HomePage.getInitialProps = async (): Promise<Props> => {
+  const title = 'Vroom: Buy, Sell or Trade-In Used Vehicles Online';
+  const description =
+    'Buy, sell or trade-in a certified used car online from anywhere in the USA. We offer no-haggle car buying, top quality cars, full warranties & home shipping.';
+  return {
+    title,
+    description,
+  };
 };
 
 export default HomePage;
