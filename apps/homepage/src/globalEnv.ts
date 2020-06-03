ea23cf5690945d6f68508ae521bcfd237b728cb0
Kevin Parrott <kevin.parrott@contractor.vroom.com>
2020-06-03 09:38:27 -0400

    reset required env var

--- apps/homepage/src/globalEnv.ts
+++ apps/homepage/src/globalEnv.ts
@@ -1,6 +1,6 @@
 export interface GlobalEnv {
-  CDN_URL: string;
-  INVSEARCH_V3_URL: string;
+  CDN_URL?: string;
+  INVSEARCH_V3_URL?: string;
 }
 
 declare global {
@@ -9,22 +9,8 @@ declare global {
   }
 }
 
-function getGlobalEnv(): GlobalEnv {
-  if (process.browser) {
-    return window.__GLOBAL_ENV__;
-  }
-  if (typeof process.env.CDN_URL === 'undefined') {
-    throw new Error('process.env.CDN_URL is undefined!');
-  }
-  if (typeof process.env.INVSEARCH_V3_URL === 'undefined') {
-    throw new Error('process.env.INVSEARCH_V3_URL is undefined!');
-  }
-  return {
-    CDN_URL: process.env.CDN_URL,
-    INVSEARCH_V3_URL: process.env.INVSEARCH_V3_URL,
-  };
-}
-
-const globalEnv: GlobalEnv = getGlobalEnv();
+const globalEnv: GlobalEnv | NodeJS.ProcessEnv = process.browser
+  ? window.__GLOBAL_ENV__
+  : process.env;
 
 export default globalEnv;
