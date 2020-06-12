bea9ffb5c1833f2243e77805dd884b05403f0cfc
Tim Laster <timothy.laster@vroom.com>
2020-06-12 15:37:07 -0400

    removed unneeded store from GalleryConditionEnd card

--- apps/product/src/modules/inventory/components/GalleryConditionEnd/index.tsx
+++ apps/product/src/modules/inventory/components/GalleryConditionEnd/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-import { InventoryStore, InventoryStoreContext } from '../../store';
 import View from './View';
 import ViewModel from './ViewModel';
 
 const GalleryConditionEnd: React.FC = () => {
-  return (
-    <InventoryStoreContext.Consumer>
-      {(store: InventoryStore): JSX.Element => {
-        const viewModel = new ViewModel(store);
-        return <View viewModel={viewModel} />;
-      }}
-    </InventoryStoreContext.Consumer>
-  );
+  const viewModel = new ViewModel();
+  return <View viewModel={viewModel} />;
 };
 
 export default GalleryConditionEnd;
