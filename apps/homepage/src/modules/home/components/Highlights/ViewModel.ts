690c50c0fb8e47938f8a2c614fdc77b7588b66fa
Collin Cromwell <collincromwell@CollinCromwell-MBPTB15-DET.local>
2020-06-25 16:08:01 -0400

    changes the description for the test

--- apps/homepage/src/modules/home/components/Highlights/ViewModel.ts
+++ apps/homepage/src/modules/home/components/Highlights/ViewModel.ts
@@ -2,6 +2,7 @@ import { HomeStore } from '../../store';
 
 import globalEnv from 'src/globalEnv';
 import AnalyticsHandler from 'src/integrations/AnalyticsHandler';
+import { getExperimentVariant } from 'src/integrations/experimentSDK';
 
 interface Highlight {
   description: string;
@@ -14,8 +15,7 @@ class HighlightsViewModel {
   readonly ctaLabel: string = 'SHOP NOW';
   readonly highlights: Highlight[] = [
     {
-      description:
-        'Multiple inspections. Free CARFAX® history report. Complimentary limited\xa0warranty.',
+      description: '',
       imgAlt: 'High-Quality Cars',
       imgSrc: `${globalEnv.CDN_URL}/modules/home/images/highlight-1.png`,
       title: 'High-Quality Cars',
@@ -39,11 +39,19 @@ class HighlightsViewModel {
   private analyticsHandler: AnalyticsHandler = new AnalyticsHandler();
 
   constructor(store: HomeStore) {
-    const browseAllVehiclesTextExperimentVaraint = getExperimentVariant(
-      'snd-homepage-browse-all-low-mileage-vs-browse-our-low-mileage',
+    const homeWarrantyTextExperimentVariant = getExperimentVariant(
+      'snd-homepage-complimentary-limited-warranty-vs-free-limited-warranty',
       store.experiments,
       store.query
     );
+    const highQualityCarHighlight = this.highlights.find(
+      (highlight) => highlight.title === 'High-Quality Cars'
+    );
+    if (highQualityCarHighlight) {
+      highQualityCarHighlight.description = `Multiple inspections. Free CARFAX® history report. ${
+        homeWarrantyTextExperimentVariant ? 'Complimentary' : 'Free'
+      } limited\xa0warranty.`;
+    }
   }
 
   handleButtonClick(): void {
