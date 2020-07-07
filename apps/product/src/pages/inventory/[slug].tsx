508c272fcf7ea884cc510df08b2ba21922d083c9
Matthew Sweda <matthew.sweda@vroom.com>
2020-07-07 15:06:00 +0000

    Merged in FIT-556 (pull request #104)
    FIT-556 Use make/model slugs on product app
    
    * FIT-556 Product canonical tag, status code, page title
    
    * Merge remote-tracking branch 'origin/master' into FIT-556
    
    * FIT-556 Product app use make/model slugs for breadcrumbs and canonical tag
    
    Approved-by: Michael Gleeson
    Approved-by: Timothy Laster
    Approved-by: Claudiu Badau

--- apps/product/src/pages/inventory/[slug].tsx
+++ apps/product/src/pages/inventory/[slug].tsx
@@ -12,17 +12,19 @@ import { Status } from 'src/networking/types';
 import Page from 'src/Page';
 
 export interface Props {
+  canonicalHref?: string;
   initialState: InventoryStoreState;
   title: string;
 }
 
 const VehicleDetails: NextPage<Props> = (props: Props) => {
-  const { initialState, title } = props;
+  const { canonicalHref, initialState, title } = props;
   const store = new InventoryStore(initialState);
   const head = (
     <>
       <title>{title}</title>)
       <meta name="robots" content="noindex, nofollow" />
+      {canonicalHref && <link rel="canonical" href={canonicalHref} />}
     </>
   );
   return (
@@ -34,21 +36,40 @@ const VehicleDetails: NextPage<Props> = (props: Props) => {
   );
 };
 
-VehicleDetails.getInitialProps = async ({ query }): Promise<Props> => {
+VehicleDetails.getInitialProps = async ({ query, res }): Promise<Props> => {
   const slug = query.slug as string;
   const slugArray = slug.split('-');
   const vin = slugArray[slugArray.length - 1];
   const initialState = await getInitialInventoryStoreState(vin);
 
+  let canonicalHref: string | undefined;
   let title = '';
   if (initialState.vehicleStatus === Status.SUCCESS) {
-    const { year, make, model } = initialState.vehicle._source;
-    title = `Used ${year} ${make} ${model} - Rocket Auto`;
+    const {
+      year,
+      make,
+      makeSlug,
+      model,
+      modelSlug,
+      listingPrice,
+    } = initialState.vehicle._source;
+    canonicalHref = `/inventory/${makeSlug}-${modelSlug}-${year}-${vin}`;
+    const currencyFormatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+    const price = currencyFormatter.format(listingPrice);
+    title = `Used ${year} ${make} ${model} For Sale (${price}) | Vroom`;
   } else {
-    title = 'Car Not Available - Rocket Auto';
+    title = 'Car Not Available | Vroom';
+    if (res) {
+      res.statusCode = 404;
+    }
   }
 
-  return { initialState, title };
+  return { canonicalHref, initialState, title };
 };
 
 export default VehicleDetails;
