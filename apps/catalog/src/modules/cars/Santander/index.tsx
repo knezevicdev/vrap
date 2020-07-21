e8ca6b8abd1d6e3cd2332c55c438064d94a417f0
Claudiu Badau <claudiu.badau@vroom.com>
2020-07-21 07:46:01 -0400

    Add layout.

--- apps/catalog/src/modules/cars/Santander/index.tsx
+++ apps/catalog/src/modules/cars/Santander/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Filters from "../Vroom/components/Filters";
+import Inventory from "../Vroom/components/Inventory";
+import {SantanderFooter} from "@vroom-web/footer-components";
+import {styled} from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const StyledGrid = styled(Grid)(() => ({
+  flexGrow: 1,
+}));
 
 const Santander: React.FC = () => {
-  return <p>Santander under development...</p>;
+  return (
+      <>
+        <StyledGrid container direction="row" wrap="nowrap">
+          <Filters />
+          <Inventory />
+        </StyledGrid>
+        <SantanderFooter />
+      </>
+  );
 };
 
 export default Santander;
