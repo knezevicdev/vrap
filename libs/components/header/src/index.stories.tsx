6f776716b38e54c8af76ff1374ec76a301df2904
Claudiu Badau <claudiu.badau@vroom.com>
2020-07-21 15:07:47 -0400

    Moving files under Vroom.

---
+++ libs/components/header/src/index.stories.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from '@vroom-web/ui';
+import React from 'react';
+
+export default { title: 'Headers' };
+
+export const Santander: React.FC = () => {
+    return (
+        <ThemeProvider>
+        </ThemeProvider>
+    );
+};
