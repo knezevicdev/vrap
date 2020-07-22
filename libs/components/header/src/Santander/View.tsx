63e174d7db9274d74ff7f46cb3fbda81758276a8
Claudiu Badau <claudiu.badau@vroom.com>
2020-07-22 09:17:56 -0400

    Adding Shop Now.

--- libs/components/header/src/Santander/View.tsx
+++ libs/components/header/src/Santander/View.tsx
@@ -1,6 +1,9 @@
 import { styled } from '@material-ui/core/styles';
+import { Typography } from '@vroom-web/ui';
 import React from 'react';
 
+import { ReactComponent as LogoSvg } from './svg/logo.svg';
+import { ReactComponent as ShopSvg } from './svg/shop.svg';
 import ViewModel from './ViewModel';
 
 interface Props {
@@ -10,11 +13,89 @@ interface Props {
 const ViewContainer = styled('div')(() => ({
   display: 'flex',
   flexDirection: 'column',
-  background: '#F1F1F1',
+}));
+
+const Top = styled('div')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  color: '#FFFFFF',
+  background: '#CC0000',
+  maxHeight: '48px',
+  minHeight: '48px',
+  padding: theme.spacing(1, 2),
+  [theme.breakpoints.only('xs')]: { padding: theme.spacing(1, 0) },
+}));
+
+const Bottom = styled('div')(({ theme }) => ({
+  display: 'flex',
+  color: '#767676',
+  background: '#FFFFFF',
+  maxHeight: '48px',
+  minHeight: '48px',
+  padding: theme.spacing(1, 2),
+  [theme.breakpoints.only('xs')]: { padding: theme.spacing(1, 3) },
+}));
+
+const Logo = styled(LogoSvg)(() => ({
+  width: '295px',
+}));
+
+const DesktopShopNow = styled('div')(({ theme }) => ({
+  display: 'flex',
+  [theme.breakpoints.only('xs')]: { display: 'none' },
+}));
+
+const MobileShopNow = styled('div')(({ theme }) => ({
+  display: 'none',
+  marginLeft: 'auto',
+  [theme.breakpoints.only('xs')]: { display: 'flex' },
+}));
+
+const ShopNowContainer = styled('a')(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  textDecoration: 'none',
+}));
+
+const ShopLabel = styled(Typography)(({ theme }) => ({
+  color: '#FFFFFF',
+  fontSize: '18px',
+  [theme.breakpoints.only('xs')]: {
+    color: '#767676',
+    fontSize: '14px',
+    fontWeight: 600,
+  },
+}));
+
+const ShopIcon = styled(ShopSvg)(({ theme }) => ({
+  width: '20px',
+  marginRight: theme.spacing(1),
+  fill: '#FFFFFF',
+  [theme.breakpoints.only('xs')]: { fill: '#767676' },
 }));
 
 const View: React.FC<Props> = ({ viewModel }) => {
-  return <ViewContainer>hello</ViewContainer>;
+  return (
+    <ViewContainer>
+      <Top>
+        <Logo />
+        <DesktopShopNow>
+          <ShopNowContainer href={viewModel.shopNow.href}>
+            <ShopIcon />
+            <ShopLabel>{viewModel.shopNow.label}</ShopLabel>
+          </ShopNowContainer>
+        </DesktopShopNow>
+      </Top>
+      <Bottom>
+        <MobileShopNow>
+          <ShopNowContainer href={viewModel.shopNow.href}>
+            <ShopIcon />
+            <ShopLabel>{viewModel.shopNow.label}</ShopLabel>
+          </ShopNowContainer>
+        </MobileShopNow>
+      </Bottom>
+    </ViewContainer>
+  );
 };
 
 export default View;
--- libs/components/header/src/Santander/ViewModel.ts
+++ libs/components/header/src/Santander/ViewModel.ts
@@ -5,6 +5,48 @@ interface Link {
   rel?: string;
 }
 
-class ViewModel {}
+class ViewModel {
+  readonly shopNow: Link = { label: 'Shop now', href: `/cars` };
+
+  readonly financeCalculators: Link = {
+    label: 'Finance Calculators',
+    href:
+      'https://santanderconsumerusa.com/learning-center/finance-calculators',
+    target: '_blank',
+  };
+  readonly learningCenterLabel: string = 'Learning Center';
+
+  readonly learningCenterLinks: Link[] = [
+    {
+      label: 'Overview',
+      href: 'https://santanderconsumerusa.com/learning-center',
+      target: '_blank',
+    },
+    {
+      label: 'Blog',
+      href: 'https://santanderconsumerusa.com/blog',
+      target: '_blank',
+    },
+    {
+      label: 'Financial Education',
+      href:
+        'https://santanderconsumerusa.com/learning-center/financial-education',
+      target: '_blank',
+    },
+    {
+      label: 'Servicemembers Civil Relief Act',
+      href:
+        'https://santanderconsumerusa.com/legal/servicemembers-civil-relief-act',
+      target: '_blank',
+    },
+  ];
+
+  readonly contactUs: Link = { label: 'Contact Us', href: `/contact` };
+
+  readonly backToCorporate: Link = {
+    label: 'Back to Corporate Site',
+    href: 'https://santanderconsumerusa.com/',
+  };
+}
 
 export default ViewModel;
