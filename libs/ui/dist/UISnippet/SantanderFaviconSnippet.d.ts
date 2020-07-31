70d33077d5ee54aaffb1bd6a318037ad127b1bf6
Kevin Parrott <kevin.parrott@vroom.com>
2020-07-31 11:32:08 -0400

    cleanup from merge

--- libs/ui/dist/UISnippet/SantanderFaviconSnippet.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Theme } from '../types';
-interface Props {
-    hostUrl: string;
-    theme: Theme;
-}
-declare const SantanderFaviconSnippet: React.FC<Props>;
-export default SantanderFaviconSnippet;
