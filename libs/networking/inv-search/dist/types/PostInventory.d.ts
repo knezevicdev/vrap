e0da84df1731031c9ec23dc180a970b0c054eb71
Collin Cromwell <collincromwell@CollinCPTB15DET.attlocal.net>
2020-08-04 11:57:35 -0400

    updates Post Inventory data type

--- libs/networking/inv-search/dist/types/PostInventory.d.ts
+++ libs/networking/inv-search/dist/types/PostInventory.d.ts
@@ -1,42 +1,49 @@
 import { InventoryResponse } from './Inventory';
 export declare enum SoldStatus {
-    ALL_CARS = "all_cars",
-    FOR_SALE = "for_sale",
-    SALE_PENDING = "sale_pending",
-    SOLD = "sold",
-    DELIVERED = "delivered"
+  ALL_CARS = 'all_cars',
+  FOR_SALE = 'for_sale',
+  SALE_PENDING = 'sale_pending',
+  SOLD = 'sold',
+  DELIVERED = 'delivered',
 }
 export interface PostInventoryRequestData {
-    offset?: number;
-    fulldetails?: boolean;
-    limit?: number;
-    testdriveonly?: boolean;
-    make?: string[];
-    makeSlug?: string[];
-    model?: string[];
-    modelSlug?: string[];
-    bodytype?: string[];
-    color?: string[];
-    year?: {} | {
+  offset?: number;
+  fulldetails?: boolean;
+  limit?: number;
+  testdriveonly?: boolean;
+  make?: string[];
+  makeSlug?: string[];
+  model?: string[];
+  modelSlug?: string[];
+  bodytype?: string[];
+  color?: string[];
+  year?:
+    | {}
+    | {
         min: number;
         max: number;
-    };
-    price?: {} | {
+      };
+  price?:
+    | {}
+    | {
         min: number;
         max: number;
-    };
-    miles?: {} | {
+      };
+  miles?:
+    | {}
+    | {
         min: number;
         max: number;
-    };
-    transmissionid?: string;
-    drivetype?: string[];
-    sortby?: string;
-    sortdirection?: string;
-    searchall?: string;
-    vin?: string[];
-    'sold-status'?: SoldStatus;
-    source?: string;
+      };
+  transmissionid?: string;
+  drivetype?: string[];
+  sortby?: string;
+  sortdirection?: string;
+  searchall?: string;
+  vin?: string[];
+  'sold-status'?: SoldStatus;
+  source?: string;
+  geo?: object;
 }
-export declare const postInventoryResponseSchema: import("yup").ObjectSchema<InventoryResponse>;
+export declare const postInventoryResponseSchema: import('yup').ObjectSchema<InventoryResponse>;
 export declare type PostInventoryResponse = InventoryResponse;
