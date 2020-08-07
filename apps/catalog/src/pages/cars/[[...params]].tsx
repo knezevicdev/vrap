09107123cb6ae2b205d599d3113af0b08fd82bea
Matthew Sweda <matthew.sweda@vroom.com>
2020-08-07 15:02:36 +0000

    Merged in DELTA-5 (pull request #179)
    DELTA-5 Dynamic catalog page titles / descriptions
    
    * DELTA-5 Dynamic catalog page titles / descriptions
    
    Approved-by: Michael Gleeson
    Approved-by: Claudiu Badau

--- apps/catalog/src/pages/cars/[[...params]].tsx
+++ apps/catalog/src/pages/cars/[[...params]].tsx
@@ -18,24 +18,21 @@ import {
   getInitialCarsStoreState,
   InitialCarsStoreState,
 } from 'src/modules/cars/store';
+import { Status } from 'src/networking/types';
 import Page from 'src/Page';
 
 interface Props {
   brand: Brand;
-  description: string;
   experiments: Experiment[];
   indexPage: boolean;
   initialStoreState: InitialCarsStoreState;
-  title: string;
 }
 
 const CarsPage: NextPage<Props> = ({
   brand,
-  description,
   experiments,
   indexPage,
   initialStoreState,
-  title,
 }) => {
   // Persist store instance across URL updates.
   const [carsStore] = useState<CarsStore>(new CarsStore(initialStoreState));
@@ -51,6 +48,141 @@ const CarsPage: NextPage<Props> = ({
       carsStore.setAreFiltersOpen(true);
     }
   }, [carsStore, theme]);
+
+  // DELTA-5
+  // This is more complex than I would like.
+  // TODO: investigate a better solution that will play nicely with chips as well.
+  const getDisplayAttributesFromUrl = (): {
+    bodyType?: string;
+    make?: string;
+    model?: string;
+    year?: string;
+  } => {
+    if (!carsStore.filtersData) {
+      return {};
+    }
+
+    let bodyType: string | undefined;
+    if (
+      carsStore.filtersData.bodytypes &&
+      carsStore.filtersData.bodytypes.length > 0 &&
+      carsStore.bodyTypes.length > 0
+    ) {
+      const bodyTypeSlug = carsStore.filtersData.bodytypes[0];
+      const matchingBodyType = carsStore.bodyTypes.find(
+        (bt) => bt.filtersDataValue === bodyTypeSlug
+      );
+      if (matchingBodyType) {
+        bodyType = matchingBodyType.display;
+      }
+    }
+
+    let make: string | undefined;
+    let model: string | undefined;
+    if (
+      carsStore.filtersData.makesandmodels &&
+      carsStore.filtersData.makesandmodels.length > 0 &&
+      carsStore.makeBuckets &&
+      carsStore.makeBuckets.length > 0
+    ) {
+      const makeAndModels = carsStore.filtersData.makesandmodels[0];
+      const makeSlug = makeAndModels.makeSlug;
+      const matchingMakeBucket = carsStore.makeBuckets.find(
+        (mb) => mb.slug === makeSlug
+      );
+      if (matchingMakeBucket) {
+        make = matchingMakeBucket.key;
+        if (
+          makeAndModels.modelSlugs &&
+          makeAndModels.modelSlugs.length > 0 &&
+          matchingMakeBucket.model_count.buckets.length > 0
+        ) {
+          const modelSlug = makeAndModels.modelSlugs[0];
+          const matchingModelBucket = matchingMakeBucket.model_count.buckets.find(
+            (mb) => mb.slug === modelSlug
+          );
+          if (matchingModelBucket) {
+            model = matchingModelBucket.key;
+          }
+        }
+      }
+    }
+
+    let year: string | undefined;
+    if (carsStore.filtersData.year) {
+      if (carsStore.filtersData.year.max !== carsStore.filtersData.year.min) {
+        year = `${carsStore.filtersData.year.min}-${carsStore.filtersData.year.max}`;
+      } else {
+        year = `${carsStore.filtersData.year.max}`;
+      }
+    }
+
+    return {
+      bodyType,
+      make,
+      model,
+      year,
+    };
+  };
+
+  const { bodyType, make, model, year } = getDisplayAttributesFromUrl();
+
+  const getTitle = (): string => {
+    if (brand === Brand.SANTANDER) {
+      return 'Shop Used Cars Online - Santander Consumer USA';
+    }
+
+    const template = (descriptor: string): string => {
+      const pluralRegExp = new RegExp('^(.*)(s|z)$');
+      const descriptorIsPlural = pluralRegExp.test(descriptor);
+      const pluralDescriptor = descriptorIsPlural
+        ? descriptor
+        : `${descriptor}s`;
+      return `Used ${pluralDescriptor} for Sale: Buy Online + Home Delivery | Vroom`;
+    };
+
+    if (make && model && year) {
+      return template(`${year} ${make} ${model}`);
+    }
+
+    if (make && model) {
+      return template(`${make} ${model}`);
+    }
+
+    if (make && year) {
+      return template(`${year} ${make}`);
+    }
+
+    if (bodyType && make) {
+      return template(`${make} ${bodyType}`);
+    }
+
+    if (make) {
+      return template(make);
+    }
+
+    if (bodyType) {
+      return template(`${bodyType}`);
+    }
+
+    return template('Cars');
+  };
+  const title = getTitle();
+
+  const getDescription = (): string => {
+    if (brand === Brand.SANTANDER) {
+      return 'Buy your next car online with Santander Consumer USA. We offer high quality cars, easy car buying, & delivery anywhere in the USA.';
+    }
+
+    const descriptorArray = [make, model, bodyType].filter((item) => !!item);
+    const descriptor =
+      descriptorArray.length > 0 ? descriptorArray.join(' ') : 'car';
+
+    return `Buy your next used ${descriptor} with Vroom. Browse our high-quality ${
+      year || ''
+    } ${descriptor} inventory, buy online, and have it delivered straight to you.`;
+  };
+  const description = getDescription();
 
   const head = (
     <>
@@ -92,7 +224,7 @@ CarsPage.getInitialProps = async (context: NextPageContext): Promise<Props> => {
     },
   } = context;
 
-  const { req, query } = context;
+  const { req, res, query } = context;
   const headerBrandKey = 'x-brand';
   const santanderKey = 'santander';
   const brandHeader = req && req.headers[headerBrandKey];
@@ -128,17 +260,6 @@ CarsPage.getInitialProps = async (context: NextPageContext): Promise<Props> => {
       speed: null,
     };
   }
-  const title =
-    brand === Brand.SANTANDER
-      ? 'Shop Used Cars Online - Santander Consumer USA'
-      : 'Buy Low-Mileage Used Cars & Trucks Online - Vroom';
-
-  const description =
-    brand === Brand.SANTANDER
-      ? 'Buy your next car online with Santander Consumer USA. We offer high quality cars, easy car buying, & delivery anywhere in the USA.'
-      : 'Buy your next car online with Vroom. We offer certified used cars for sale, no haggle car buying, full warranties and home shipping anywhere in the USA.';
-
-  const indexPage = brand === Brand.SANTANDER ? true : false;
 
   const experiments =
     brand === Brand.VROOM
@@ -184,13 +305,29 @@ CarsPage.getInitialProps = async (context: NextPageContext): Promise<Props> => {
     geo,
     url
   );
+
+  const getHasInventory = (): boolean => {
+    if (initialStoreState.inventoryStatus !== Status.SUCCESS) {
+      return false;
+    }
+    if (!initialStoreState.inventoryData) {
+      return false;
+    }
+    return initialStoreState.inventoryData.hits.total !== 0;
+  };
+  const hasInventory = getHasInventory();
+
+  if (res && !hasInventory) {
+    res.statusCode = 404;
+  }
+
+  const indexPage = brand === Brand.SANTANDER ? true : false;
+
   return {
     brand,
-    description,
     experiments,
     indexPage,
     initialStoreState,
-    title,
   };
 };
 
