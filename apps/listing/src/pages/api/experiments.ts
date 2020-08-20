091336607046b99ceda3b96ecba0ab2b409250a9
Claudiu Badau <claudiu.badau@vroom.com>
2020-08-20 14:17:25 -0400

    Linting.

--- apps/listing/src/pages/api/experiments.ts
+++ apps/listing/src/pages/api/experiments.ts
@@ -1,9 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import experimentSDK from "src/integrations/experimentSDK";
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-    const {cookies} = req;
-    const marketingId = cookies['uuid'];
-    const experiments = await experimentSDK.getActiveExperiments(marketingId);
-    res.status(200).json(experiments);
+import experimentSDK from 'src/integrations/experimentSDK';
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  const { cookies } = req;
+  const marketingId = cookies['uuid'];
+  const experiments = await experimentSDK.getActiveExperiments(marketingId);
+  res.status(200).json(experiments);
 };
