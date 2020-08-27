8a8f7dbf4303d794565b733e73f6b900d7db7fc1
Timothy Laster <timothy.laster@vroom.com>
2020-08-27 18:28:58 +0000

    Merged in SND-1047 (pull request #218)
    SND-1047: Vehicle Size - Unavailable on 0 return
    
    * refactor vehicleSizes
    
    * made typing better
    
    * added to santander
    
    Approved-by: Collin Cromwell
    Approved-by: Michael Gleeson
    Approved-by: Claudiu Badau

---
+++ apps/product/src/modules/inventory/Santader/components/CarDetails/components/Performance/ViewModel.ts
@@ -0,0 +1,54 @@
+import { Car } from '@vroom-web/inv-search-networking';
+
+import { InventoryStore } from 'src/modules/inventory/store';
+
+interface PerformanceSection {
+  title: string;
+  items: { label: string; value: string }[];
+  recallLink: { text: string; href: string };
+}
+class ViewModel {
+  private car: Car;
+
+  constructor(store: InventoryStore) {
+    this.car = store.vehicle._source;
+  }
+
+  getInformation = (): PerformanceSection => {
+    return {
+      title: 'Performance',
+      items: [
+        {
+          label: 'Engine',
+          value: this.car.engine,
+        },
+        {
+          label: 'Transmission',
+          value: this.car.transmission,
+        },
+        {
+          label: 'Drive Type',
+          value: this.car.driveType,
+        },
+        {
+          label: 'Fuel Type',
+          value: this.car.fuelType,
+        },
+        ...(this.car.cityMpg > 0 && this.car.highwayMpg > 0
+          ? [
+              {
+                label: 'MPG',
+                value: `${this.car.cityMpg} City / ${this.car.highwayMpg} Hwy`,
+              },
+            ]
+          : []),
+      ],
+      recallLink: {
+        text: 'Check for Safety Recalls',
+        href: `https://www.nhtsa.gov/recalls?vin=${this.car.vin}`,
+      },
+    };
+  };
+}
+
+export default ViewModel;
