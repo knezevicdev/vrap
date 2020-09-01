be1c51ffb6fdb005c039ce45652cee999cb6c3e7
Matthew Sweda <matthew.sweda@vroom.com>
2020-09-01 18:23:07 +0000

    Merged in DELTA-115 (pull request #225)
    DELTA-115 Landing App
    
    * DELTA-115 Landing App
    
    * DELTA-115 Landing App Deploy Scripts
    
    Approved-by: Michael Gleeson
    Approved-by: Mike Rizzo

---
+++ apps/landing/src/integrations/AnalyticsHandler.ts
@@ -0,0 +1,7 @@
+import { AnalyticsHandler as BaseAnalyticsHandler } from '@vroom-web/analytics-integration';
+
+class AnalyticsHandler extends BaseAnalyticsHandler {
+  // TODO: add landing page analytics events here.
+}
+
+export default AnalyticsHandler;
