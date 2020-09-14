ca5470ad0c9845bb7363bceac636423b44e24cf6
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2020-09-14 14:16:20 -0400

    Added isAvailableSoon filter to the Get Similar Vehicles endpoint which will be false so that similar vehicles does not show any available soon cars

--- libs/networking/inv-search/src/types/GetInventorySimilar.ts
+++ libs/networking/inv-search/src/types/GetInventorySimilar.ts
@@ -4,6 +4,7 @@ export interface GetInventorySimilarRequestData {
   min: number;
   vin: string;
   useVinCluster: boolean;
+  isAvailableSoon: boolean;
 }
 
 export const getInventorySimilarResponseSchema = inventoryResponseSchema;
