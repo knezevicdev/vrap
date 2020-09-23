aa2d6201a33b122e867c874e43f549f76b653c26
Collin Cromwell <collin.cromwell@vroom.com>
2020-09-23 15:02:09 -0400

    unit tests for inventory store

--- libs/networking/inv-search/src/InvSearchNetworker.test.ts
+++ libs/networking/inv-search/src/InvSearchNetworker.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import axios, { AxiosInstance } from 'axios';
 import { mocked } from 'ts-jest/utils';
 
@@ -11,9 +12,9 @@ const mockAxios: AxiosInstance = jest.genMockFromModule('axios');
 
 describe('getInventoryCount', () => {
   describe('data validation', () => {
+    mocked(axios.create).mockImplementationOnce(() => mockAxios);
+    const invSearchNetworker = new InvSearchNetworker('testUrl');
     it('resolves data that matches the schema', async () => {
-      mocked(axios.create).mockImplementationOnce(() => mockAxios);
-      const invSearchNetworker = new InvSearchNetworker('testUrl');
       const data = {
         data: 42,
       };
@@ -28,8 +29,6 @@ describe('getInventoryCount', () => {
     });
 
     it('rejects data that does not match the schema - 1', async () => {
-      mocked(axios.create).mockImplementationOnce(() => mockAxios);
-      const invSearchNetworker = new InvSearchNetworker('testUrl');
       mocked(mockAxios.get).mockImplementationOnce(() =>
         Promise.resolve({
           data: 42,
@@ -39,8 +38,6 @@ describe('getInventoryCount', () => {
     });
 
     it('rejects data that does not match the schema - 2', async () => {
-      mocked(axios.create).mockImplementationOnce(() => mockAxios);
-      const invSearchNetworker = new InvSearchNetworker('testUrl');
       mocked(mockAxios.get).mockImplementationOnce(() =>
         Promise.resolve({
           data: {
@@ -49,6 +46,38 @@ describe('getInventoryCount', () => {
         })
       );
       await expect(invSearchNetworker.getInventoryCount()).rejects.toThrow();
+    });
+  });
+});
+
+describe('getInventorySimilar', () => {
+  mocked(axios.create).mockImplementationOnce(() => mockAxios);
+  const invSearchNetworker = new InvSearchNetworker('testUrl');
+  describe('data validation', () => {
+    it('rejects data that does not match the schema - 1', async () => {
+      mocked(mockAxios.get).mockImplementationOnce(() =>
+        Promise.resolve({
+          data: {
+            timed_out: false,
+            hits: { total: 0, hits: null },
+            aggregations: {
+              make_count: { buckets: null },
+              makeKey_count: { buckets: null },
+              transId_count: { buckets: null },
+              drivetrain_count: { buckets: null },
+              bodyType_count: { buckets: null },
+            },
+          },
+        })
+      );
+      const mockData = {
+        min: 1,
+        vin: '',
+        useVinCluster: true,
+      };
+      await expect(
+        invSearchNetworker.getInventorySimilar(mockData)
+      ).rejects.toThrow();
     });
   });
 });
