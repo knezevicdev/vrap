6bf1fe1da750d3c115596fd8866eeb77ce8c6dc0
Claudiu Badau <claudiu.badau@vroom.com>
2020-09-28 13:18:01 -0400

    Colors VM.

--- apps/landing/src/modules/index.tsx
+++ apps/landing/src/modules/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Footer from '../core/Footer';
-import { Colors } from './sections/Colors';
+import Colors from './sections/Colors';
 import { Features } from './sections/Features';
 import { Header } from './sections/Header';
 import Hero from './sections/Hero';
