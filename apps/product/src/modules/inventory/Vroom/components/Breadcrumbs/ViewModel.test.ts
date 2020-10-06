3d0067ccb3864365247ba7ca80713b8d101cc966
Sankalp Heranjal <sankalpheranjal@SankalpP15TBDET.attlocal.net>
2020-10-06 15:09:36 -0400

    added a car test file so we dont have to create it everytime

--- apps/product/src/modules/inventory/Vroom/components/Breadcrumbs/ViewModel.test.ts
+++ apps/product/src/modules/inventory/Vroom/components/Breadcrumbs/ViewModel.test.ts
@@ -1,6 +1,7 @@
 import { Car } from '@vroom-web/inv-search-networking';
 import ViewModel from './ViewModel';
 import { InventoryStore } from 'src/modules/inventory/store';
+import data from '../../testCar.json';
 jest.mock('next/config', () => {
   return (): unknown => {
     return {
@@ -10,66 +11,7 @@ jest.mock('next/config', () => {
 });
 
 describe('Breadcrumbs ViewModel', () => {
-  const car: Car = {
-    vin: 'some-vin',
-    bodyType: '',
-    interiorPhotoUrl: '',
-    diesel: 0,
-    leadFlagPhotoUrl: '',
-    listingPrice: 0,
-    color: '',
-    year: 2017,
-    leadFlagPhotoUrlHiRes: '',
-    subjectLine: '',
-    warrantyRemaining: '',
-    miles: 0,
-    interiorPhotoUrlHiRes: '',
-    dvd: 0,
-    transmission: '',
-    trim: '',
-    engine: '',
-    hiresPhotos: [],
-    warranty: 0,
-    model: 'Impreza',
-    modelSlug: 'impreza',
-    extColor: '',
-    text: '',
-    engId: 0,
-    bodyId: 0,
-    make: 'Subaru',
-    makeSlug: 'subaru',
-    vehicleType: '',
-    doorCount: 0,
-    roof: 0,
-    nav: 0,
-    warrantyOriginal: '',
-    driveType: '',
-    intColor: '',
-    cylinders: 0,
-    awd: 0,
-    fuelType: '',
-    leadPhotoUrlHiRes: '',
-    leadPhotoUrl: '',
-    style: '',
-    optionalFeatures: '',
-    zone: '',
-    soldStatus: 0,
-    otherPhotos: null,
-    defectPhotos: null,
-    ownerCount: 1,
-    cityMpg: 0,
-    highwayMpg: 0,
-    inventoryId: 0,
-    frontTrackWidth: 0,
-    rearTrackWidth: 0,
-    wheelBase: 0,
-    width: 0,
-    length: 0,
-    groundClearance: 0,
-    height: 0,
-    hasStockPhotos: false,
-    consignmentPartnerId: '',
-  };
+  const car: Car = data;
 
   it('should return the correct breadcrumbs for a given car', () => {
     const attributionQS = '?utm_source=source&utm_campaign=campaign';
--- apps/product/src/modules/inventory/Vroom/components/Favorites/ViewModel.test.ts
+++ apps/product/src/modules/inventory/Vroom/components/Favorites/ViewModel.test.ts
@@ -1,4 +1,5 @@
 import { Car } from '@vroom-web/inv-search-networking';
+import data from '../../testCar.json';
 
 import FavoritesNetworker, {
   AddFavroiteResponse,
@@ -18,66 +19,7 @@ jest.mock('next/config', () => {
   };
 });
 
-const car: Car = {
-  vin: 'some-vin',
-  bodyType: '',
-  interiorPhotoUrl: '',
-  diesel: 0,
-  leadFlagPhotoUrl: '',
-  listingPrice: 0,
-  color: '',
-  year: 0,
-  leadFlagPhotoUrlHiRes: '',
-  subjectLine: '',
-  warrantyRemaining: '',
-  miles: 0,
-  interiorPhotoUrlHiRes: '',
-  dvd: 0,
-  transmission: '',
-  trim: '',
-  engine: '',
-  hiresPhotos: [],
-  warranty: 0,
-  model: '',
-  modelSlug: '',
-  extColor: '',
-  text: '',
-  engId: 0,
-  bodyId: 0,
-  make: '',
-  makeSlug: '',
-  vehicleType: '',
-  doorCount: 0,
-  roof: 0,
-  nav: 0,
-  warrantyOriginal: '',
-  driveType: '',
-  intColor: '',
-  cylinders: 0,
-  awd: 0,
-  fuelType: '',
-  leadPhotoUrlHiRes: '',
-  leadPhotoUrl: '',
-  style: '',
-  optionalFeatures: '',
-  zone: '',
-  soldStatus: 0,
-  otherPhotos: null,
-  defectPhotos: null,
-  ownerCount: 1,
-  cityMpg: 0,
-  highwayMpg: 0,
-  inventoryId: 0,
-  frontTrackWidth: 0,
-  rearTrackWidth: 0,
-  wheelBase: 0,
-  width: 0,
-  length: 0,
-  groundClearance: 0,
-  height: 0,
-  hasStockPhotos: false,
-  consignmentPartnerId: '',
-};
+const car: Car = data;
 
 describe('Favorites View Model', () => {
   const inventoryStore = new InventoryStore();
