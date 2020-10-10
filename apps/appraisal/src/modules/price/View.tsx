2c3506d35d670480b3a64587fa7e238a3d9007f2
Joseph Cho <jscho13@gmail.com>
2020-10-10 15:21:19 -0400

    add some icons

--- apps/appraisal/src/modules/price/View.tsx
+++ apps/appraisal/src/modules/price/View.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
-
 import InitialPrice from './components/InitialPrice';
 import NextSteps from './components/NextSteps';
 import PriceViewModel, { PriceStatus } from './ViewModel';
+import React from 'react';
+import getConfig from 'next/config';
+import styled from 'styled-components';
+
+const {
+  publicRuntimeConfig: { BASE_PATH },
+} = getConfig();
 
 interface Props {
   viewModel: PriceViewModel;
 }
 
-const BackgroundContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
+const HeroContainer = styled.div`
+  background-color: #f5f5f5;
   width: 100%;
-  position: relative;
+  background-size: 100% 50%;
+  background-repeat: no-repeat;
+  background-image: url('${BASE_PATH}/images/offer-hero.png');
 `;
 
 const PriceContainer = styled.div`
-  border: 1px solid;
   display: flex;
-  justify-content: space-between;
+  margin: 50px;
+  justify-content: center;
+  position: relative;
+`;
+
+const PriceDetailContainer = styled.div`
+	flex-basis: 60%;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.15);
+  margin: 10px;
+`;
+
+const NextStepsContainer = styled.div`
+	flex-basis: 40%;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.15);
+  margin: 10px;
 `;
 
 const PriceView: React.FC<Props> = ({ viewModel }) => {
@@ -33,12 +50,16 @@ const PriceView: React.FC<Props> = ({ viewModel }) => {
   }
 
   return (
-    <BackgroundContainer>
+    <HeroContainer>
       <PriceContainer>
-        {priceComponent}
-        <NextSteps />
+        <PriceDetailContainer>
+          {priceComponent}
+        </PriceDetailContainer>
+        <NextStepsContainer>
+          <NextSteps />
+        </NextStepsContainer>
       </PriceContainer>
-    </BackgroundContainer>
+    </HeroContainer>
   );
 };
 
--- apps/appraisal/src/modules/price/components/NextSteps/View.tsx
+++ apps/appraisal/src/modules/price/components/NextSteps/View.tsx
@@ -1,40 +1,38 @@
-import { styled } from '@material-ui/core/styles';
-import { Container } from '@vroom-web/ui';
 import React from 'react';
-
+import styled from 'styled-components';
 import NextStepsViewModel from './ViewModel';
-
 import { Body, Hero, Title } from 'src/core/Typography';
 
-const StyledContainer = styled(Container)(() => ({
-  backgroundColor: 'white',
-}));
+const StyledContainer = styled.div`
+  background-color: white;
+  height: 100%;
+  width: 100%;
+`;
 
-const ColoredBullet = styled('div')(() => ({
-  margin: '0',
-  padding: '0 30px',
-  counterIncrement: 'section',
-  position: 'relative',
-  borderLeft: '1px solid red',
-  marginRight: '30px',
+const ColoredBullet = styled.div`
+  margin: 0;
+  padding: 0 30px;
+  counter-increment: section;
+  position: relative;
+  border-left: 1px solid red;
+  margin-left: 30px;
 
-  '&:before': {
-    content: 'counter(section)',
-    left: '0',
-    marginLeft: '-10px',
-    position: 'absolute',
-    borderRadius: '50%',
-    height: '18px',
-    width: '18px',
-    backgroundColor: 'red',
-    textAlign: 'center',
-    color: 'white',
-  },
+  &:before {
+    content: counter(section);
+    left: 0;
+    position: absolute;
+    border-radius: 50%;
+    height: 18px;
+    width: 18px;
+    background-color: red;
+    text-align: center;
+    color: white;
+  }
 
-  '&:last-child': {
-    borderLeft: '0',
-  },
-}));
+  &:last-child {
+    border-left: 0;
+  }
+`;
 
 export interface Props {
   viewModel: NextStepsViewModel;
