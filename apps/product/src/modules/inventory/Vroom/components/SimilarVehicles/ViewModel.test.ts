bf5b008314522550bd4b34d56d808dce7f43ac46
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2020-10-14 17:01:27 -0400

    Fixed Test that was failing for Similar Vehicles

--- apps/product/src/modules/inventory/Vroom/components/SimilarVehicles/ViewModel.test.ts
+++ apps/product/src/modules/inventory/Vroom/components/SimilarVehicles/ViewModel.test.ts
@@ -2,8 +2,9 @@ import { Car, Hit } from '@vroom-web/inv-search-networking';
 
 import SimilarVehiclesViewModel from './ViewModel';
 
-import AnalyticsHandler from 'src/integrations/AnalyticsHandler';
 import { InventoryStore } from 'src/modules/inventory/store';
+
+import data from '../../testCar.json';
 
 jest.mock('src/integrations/AnalyticsHandler');
 jest.mock('next/config', () => {
@@ -14,72 +15,10 @@ jest.mock('next/config', () => {
   };
 });
 
-const source: Car = {
-  vin: 'some-vin',
-  bodyType: '',
-  interiorPhotoUrl: '',
-  diesel: 0,
-  leadFlagPhotoUrl: '',
-  listingPrice: 0,
-  color: '',
-  year: 0,
-  leadFlagPhotoUrlHiRes: '',
-  subjectLine: '',
-  warrantyRemaining: '',
-  miles: 0,
-  interiorPhotoUrlHiRes: '',
-  dvd: 0,
-  transmission: '',
-  trim: '',
-  engine: '',
-  hiresPhotos: [],
-  warranty: 0,
-  model: '',
-  modelSlug: '',
-  extColor: '',
-  text: '',
-  engId: 0,
-  bodyId: 0,
-  make: '',
-  makeSlug: '',
-  vehicleType: '',
-  doorCount: 0,
-  roof: 0,
-  nav: 0,
-  warrantyOriginal: '',
-  driveType: '',
-  intColor: '',
-  cylinders: 0,
-  awd: 0,
-  fuelType: '',
-  leadPhotoUrlHiRes: '',
-  leadPhotoUrl: '',
-  style: '',
-  optionalFeatures: '',
-  zone: '',
-  soldStatus: 0,
-  otherPhotos: null,
-  defectPhotos: null,
-  ownerCount: 1,
-  cityMpg: 0,
-  highwayMpg: 0,
-  inventoryId: 0,
-  frontTrackWidth: 0,
-  rearTrackWidth: 0,
-  wheelBase: 0,
-  width: 0,
-  length: 0,
-  groundClearance: 0,
-  height: 0,
-  hasStockPhotos: false,
-  consignmentPartnerId: '',
-};
-
 describe('Similar Vehicles View Model', () => {
   describe('getCars', () => {
     const store = new InventoryStore();
-    const analyticsHandler = new AnalyticsHandler();
-    const viewModel = new SimilarVehiclesViewModel(store, analyticsHandler);
+    const viewModel = new SimilarVehiclesViewModel(store);
 
     test('getCars when null similar cars, should return []', () => {
       expect(viewModel.getCars()).toEqual([]);
@@ -93,22 +32,21 @@ describe('Similar Vehicles View Model', () => {
     test('getCars when similar cars > 0, should return those similar cars', () => {
       const similar: Hit[] = [
         {
-          _source: source,
+          _source: data,
         },
       ];
       store.similar = similar;
-      const expected: Car[] = [source];
+      const expected: Car[] = [data];
       expect(viewModel.getCars()).toEqual(expected);
     });
 
     test('getCars when similar cars > 4, should only return 4 cars', () => {
       const similar: Hit[] = Array(7).fill({
-        _source: source,
+        _source: data,
       });
       store.similar = similar;
-      const expected: Car[] = Array(4).fill(source);
+      const expected: Car[] = Array(4).fill(data);
       expect(viewModel.getCars()).toEqual(expected);
-      expect(analyticsHandler.trackProductListViewed).toBeCalled();
     });
   });
 });
