b480e16b795633f6d4f1fa5692063654ac68eb5a
Tim Laster <timothy.laster@vroom.com>
2020-10-18 13:33:41 -0400

    add CarDetails viewmodel tests

--- apps/product/jest.config.js
+++ apps/product/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
+    '!**/src/modules/inventory/Santader/**',
   ],
   // setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: [
