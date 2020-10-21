ce3c316aa697ece41f1022fa069230a6d22c447b
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2020-10-21 15:17:40 -0400

    Copied the Marketing Contact Vroom folder into a new TDA folder

---
+++ apps/marketing/src/modules/contact/TDA/components/ContactLabel/ViewModel.tsx
@@ -0,0 +1,8 @@
+class ContactViewModel {
+  readonly title: string = 'Contact Us';
+  readonly supportText: string = `We'd be happy to help. Give us a call at `;
+  readonly phone: string = '(877) 916-1000';
+  readonly speak: string = ' to speak with a representative.';
+}
+
+export default ContactViewModel;
