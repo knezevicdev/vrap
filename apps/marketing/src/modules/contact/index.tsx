ba028b327aec97e0838570ab694297802084efc1
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2020-10-21 15:19:40 -0400

    Updated index.ts to check for TDA brand and render the TDA Contact Component

--- apps/marketing/src/modules/contact/index.tsx
+++ apps/marketing/src/modules/contact/index.tsx
@@ -3,12 +3,16 @@ import React, { useContext } from 'react';
 
 import { BrandContext } from './BrandContext';
 import Santander from './Santander';
+import TDA from './TDA';
 import Vroom from './Vroom';
 
 const Contact: React.FC = () => {
   const brand = useContext(BrandContext);
   if (brand === Brand.SANTANDER) {
     return <Santander />;
+  }
+  if (brand === Brand.TDA) {
+    return <TDA />;
   }
   return <Vroom />;
 };
