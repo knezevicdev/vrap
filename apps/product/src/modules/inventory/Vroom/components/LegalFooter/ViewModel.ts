dad3eb9a357489c5bc59ddb4058b1f5099e00a2d
Tim Laster <timothy.laster@vroom.com>
2020-10-26 17:23:11 -0400

    update legal footer text

--- apps/product/src/modules/inventory/Vroom/components/LegalFooter/ViewModel.ts
+++ apps/product/src/modules/inventory/Vroom/components/LegalFooter/ViewModel.ts
@@ -1,6 +1,8 @@
 class LegalFooterViewModel {
-  readonly legalText: string =
-    '* Prices are subject to change and exclude all tax, title, tags, and other fees, which will be calculated at the time of purchase. We make every effort to provide accurate vehicle information on this page, but please verify before purchasing.';
+  readonly legalText: string[] = [
+    'Price displayed does not include: Pre-delivery service charges of $285.50 (MA residents only - $300.50), Delivery charge of $599, FL residents only - Electronic registration filing charge of $8.00, or applicable taxes, title, tag and registration charges which will be calculated at the time of purchase.  Prices are subject to change.',
+    'We make every effort to provide accurate vehicle information on this page, but please verify before purchasing.',
+  ];
 }
 
 export default LegalFooterViewModel;
