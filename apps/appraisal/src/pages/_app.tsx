c6d9439b04ad3d4a22012592c977e3c443baece0
Joseph Cho <jscho13@gmail.com>
2020-11-02 23:28:01 -0500

    use envs for datadog

--- apps/appraisal/src/pages/_app.tsx
+++ apps/appraisal/src/pages/_app.tsx
@@ -1,23 +1,21 @@
 import { datadogRum } from '@datadog/browser-rum';
 import { Brand, ThemeProvider } from '@vroom-web/ui';
 import App from 'next/app';
-import getConfig from 'next/config';
 import { name, version } from 'package.json';
 import React from 'react';
 import smoothscroll from 'smoothscroll-polyfill';
 import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
 
+import ENVS from 'src/integrations/Envs';
 import { GlobalStyle, theme } from '../core/themes/Vroom';
-
-const { publicRuntimeConfig } = getConfig();
 
 class VroomApp extends App {
   componentDidMount(): void {
     smoothscroll.polyfill(); // needs access to the window
-    if (publicRuntimeConfig.DATA_DOG_RUM_APPLICATION) {
+    if (ENVS.DATA_DOG_RUM_APPLICATION) {
       datadogRum.init({
-        applicationId: publicRuntimeConfig.DATA_DOG_RUM_APPLICATION,
-        clientToken: publicRuntimeConfig.DATA_DOG_RUM_TOKEN,
+        applicationId: ENVS.DATA_DOG_RUM_APPLICATION,
+        clientToken: ENVS.DATA_DOG_RUM_TOKEN,
         site: 'datadoghq.com',
         service: name,
         version: version,
