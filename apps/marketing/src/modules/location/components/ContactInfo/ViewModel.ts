1cc3668f7d1a28139eb23f93c4d3574c6653c62a
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2020-11-03 13:41:03 -0500

    Added VIT params to the See my Cars worth button in the marketing-location app

--- apps/marketing/src/modules/location/components/ContactInfo/ViewModel.ts
+++ apps/marketing/src/modules/location/components/ContactInfo/ViewModel.ts
@@ -5,7 +5,8 @@ class ContactInfoViewModel {
     this.locationInfo = locationInfo;
   }
   readonly title = `Selling? We're buying!`;
-  readonly suycLink = '/sell';
+  readonly suycLink =
+    '/sell?vit_source=texasdirectauto&vit_medium=wl&vit_dest=vroom&vit_brand=TDA';
   readonly buttonLabel = 'See what my car’s worth';
   readonly locationInfo: LocationInfo;
 }
