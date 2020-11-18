2591faa4ef649a86e74c367f561d9585294c4407
Matthew Sweda <matthew.sweda@vroom.com>
2020-11-18 22:01:15 +0000

    Merged in ECOMM-2752 (pull request #419)
    ECOMM-2752 Checkout - create barebones congrats page
    
    * ECOMM-2752 Checkout - create barebones congrats page
    
    * ECOMM-2752 Remove unused argument
    
    Approved-by: Sukada Ramakrishna
    Approved-by: Victor Santos
    Approved-by: Claudiu Badau

--- apps/checkout/babel.config.js
+++ apps/checkout/babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
+    [
+      'next/babel',
+      {
+        'class-properties': {
+          loose: true,
+        },
+      },
+    ],
   ],
 };
