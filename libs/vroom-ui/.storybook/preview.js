4b571ee57b196b2a830a69d0e8464aa952b1aa9a
Claudiu Badau <claudiu.badau@vroom.com>
2020-11-20 09:12:25 -0500

    Injecting path for fonts.

--- libs/vroom-ui/.storybook/preview.js
+++ libs/vroom-ui/.storybook/preview.js
@@ -1,19 +1,20 @@
-import { addDecorator } from '@storybook/react';
+import {addDecorator} from '@storybook/react';
 import React from 'react';
-import {GlobalStyle, theme} from "../src/foundation/themes/Vroom";
+import {GlobalStyle, getTheme} from "../src/foundation/themes/Vroom";
 import {ThemeProvider} from "styled-components";
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: {argTypesRegex: "^on[A-Z].*"},
 }
 
 function withGlobalStyles(storyFn) {
-  return (
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        {storyFn()}
-      </ThemeProvider>
-  );
+    const theme = getTheme('/assets');
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle/>
+            {storyFn()}
+        </ThemeProvider>
+    );
 }
 
 addDecorator(withGlobalStyles);
--- libs/vroom-ui/src/foundation/themes/Vroom.ts
+++ libs/vroom-ui/src/foundation/themes/Vroom.ts
@@ -8,42 +8,52 @@ export interface ThemeProps {
       body: string;
     };
     color: string;
+    path: string;
+  };
+  colors: {
+    [name: string]: {
+      [name: string]: string;
+    };
   };
 }
 
-export const theme = {
-  typography: {
-    family: {
-      heading: 'Vroom Sans',
-      title: 'Calibre',
-      body: 'Calibre',
+export const getTheme = (fontPath: string): ThemeProps => {
+  return {
+    typography: {
+      family: {
+        heading: 'Vroom Sans',
+        title: 'Calibre',
+        body: 'Calibre',
+      },
+      color: '#041022',
+      path: fontPath,
     },
-    color: '#041022',
-  },
-  colors: {
-    primary: {
-      red: '#E7131A',
-      black: '#041022',
-      white: '#FFFFFF',
+    colors: {
+      primary: {
+        red: '#E7131A',
+        black: '#041022',
+        white: '#FFFFFF',
+      },
+      secondary: {
+        blue: '#1960D0',
+        green: '#308406',
+        orange: '#F26900',
+        yellow: '#FFD400',
+        red: '#FC4349',
+        pink: '#FEE8E9',
+      },
+      neutral: {
+        gray1: '#6C717A',
+        gray2: '#999DA3',
+        gray3: '#D6D7DA',
+        gray4: '#F5F5F5',
+      },
     },
-    secondary: {
-      blue: '#1960D0',
-      green: '#308406',
-      orange: '#F26900',
-      yellow: '#FFD400',
-      red: '#FC4349',
-      pink: '#FEE8E9',
-    },
-    neutral: {
-      gray1: '#6C717A',
-      gray2: '#999DA3',
-      gray3: '#D6D7DA',
-      gray4: '#F5F5F5',
-    },
-  },
+  };
 };
 
-const BASE_PATH = '/assets';
+const path = ({ theme: { typography } }: { theme: ThemeProps }): string =>
+  typography.path;
 
 export const GlobalStyle = createGlobalStyle`
   html {
@@ -58,19 +68,19 @@ export const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: Calibre;
     font-weight: normal;
-    src: url(${BASE_PATH}/fonts/Vroom/Calibre-Regular.woff2) format('woff2');
+    src: url(${path}/fonts/Vroom/Calibre-Regular.woff2) format('woff2');
     font-display: swap;
   }
   @font-face {
     font-family: Calibre;
     font-weight: 600;
-    src: url(${BASE_PATH}/fonts/Vroom/Calibre-Semibold.woff2) format('woff2');
+    src: url(${path}/fonts/Vroom/Calibre-Semibold.woff2) format('woff2');
     font-display: swap;
   }
   @font-face {
     font-family: Vroom Sans;
     font-weight: normal;
-    src: url(${BASE_PATH}/fonts/Vroom/Vroom-Sans.woff2) format('woff2');
+    src: url(${path}/fonts/Vroom/Vroom-Sans.woff2) format('woff2');
     font-display: swap;
   }
   
