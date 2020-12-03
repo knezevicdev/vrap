a9d32070c86308efb7b5879f478e705f8001b6a0
Sasha Kondrashov <timofey.kondrashov@vroom.com>
2020-12-03 09:32:20 -0500

    create signup page and module

---
+++ apps/logistics/src/modules/register/Model.ts
@@ -0,0 +1,87 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+
+import { Carrier, User } from 'src/networking/models/User';
+import {
+  getUsers,
+  getUserStatuses,
+  patchUser,
+  Status,
+} from 'src/networking/Networker';
+
+class RegisterModel {
+  users: User[] = [];
+  usersStatus: Status = Status.INITIAL;
+
+  carrierFilter?: Carrier;
+  statusFilter?: string;
+  statusOptions: string[] = [];
+  statusOptionsStatus: Status = Status.INITIAL;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  getUsers = async (): Promise<void> => {
+    this.usersStatus = Status.FETCHING;
+
+    try {
+      const response = await getUsers(
+        this.carrierFilter?.carrier_code ?? undefined,
+        this.statusFilter
+      );
+
+      runInAction(() => {
+        this.usersStatus = Status.SUCCESS;
+        this.users = response.data;
+      });
+    } catch (err) {
+      console.error(err);
+
+      this.usersStatus = Status.ERROR;
+    }
+  };
+
+  patchUser = async (
+    id: number,
+    status?: string,
+    carrierCode?: string
+  ): Promise<void> => {
+    try {
+      const response = await patchUser(id, status, carrierCode);
+      runInAction(() => {
+        const index = this.users.findIndex(
+          (i) => i.portal_user_id === response.data.portal_user_id
+        );
+        this.users[index] = response.data;
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // statuses to assign to / filter a user
+  getUserStatuses = async (): Promise<void> => {
+    this.statusOptionsStatus = Status.FETCHING;
+    try {
+      const response = await getUserStatuses();
+
+      runInAction(() => {
+        this.statusOptionsStatus = Status.SUCCESS;
+        this.statusOptions = response.data;
+      });
+    } catch (err) {
+      console.error(err);
+      this.statusOptionsStatus = Status.ERROR;
+    }
+  };
+
+  setCarrierFilter = (value: Carrier | undefined): void => {
+    this.carrierFilter = value;
+  };
+
+  setStatusFilter = (value: string): void => {
+    this.statusFilter = value;
+  };
+}
+
+export default RegisterModel;
---
+++ apps/logistics/src/modules/register/View.tsx
@@ -0,0 +1,71 @@
+import {
+  Box,
+  Button,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import React, { useEffect } from 'react';
+
+import ViewModel from './ViewModel';
+
+interface Props {
+  viewModel: ViewModel;
+}
+
+const UsersView: React.FC<Props> = ({ viewModel }) => {
+  useEffect(() => {
+    viewModel.getUserStatuses();
+  }, [viewModel]);
+
+  return (
+    <div>
+      <Box p={4} width="40%" mx="auto">
+        <Paper square>
+          <form>
+            <Grid container direction="column" spacing={2}>
+              <Grid item container justify="center">
+                <TextField
+                  required
+                  label="Email"
+                  type="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item container justify="center">
+                <TextField
+                  required
+                  label="Password"
+                  type="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item container justify="center">
+                <TextField
+                  required
+                  label="Confirm Password"
+                  type="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item container justify="center">
+                <Button variant="contained" color="primary">
+                  Sign Up
+                </Button>
+              </Grid>
+              <Grid item container justify="center">
+                <Typography>
+                  <Link href="/login">Already signed up? Log in here!</Link>
+                </Typography>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </Box>
+    </div>
+  );
+};
+
+export default UsersView;
