b149033d49f1808c7fba93f8dc682dc05df7e7cb
Claudiu Badau <claudiu.badau@vroom.com>
2020-12-04 08:08:24 -0500

    Initial purchase summary.

--- apps/checkout/src/modules/congrats/sections/PurchaseSummary/View.tsx
+++ apps/checkout/src/modules/congrats/sections/PurchaseSummary/View.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
 
 import ViewModel from './ViewModel';
+import styled from "styled-components";
+import { Heading, Body, Title } from 'vroom-ui/src/foundation/Typography';
+import {Picture} from "vroom-ui/src/elements/Picture";
 
 interface Props {
-  viewModel: ViewModel;
+    viewModel: ViewModel;
 }
 
+const Container = styled.div`
+  display:flex;
+  flex-direction: column;
+  max-width: 640px;
+`;
+
+const Car = styled.div`
+  display:flex;
+  max-height: 104px;
+`;
+
+const CarInfo = styled.div`
+  display:flex;
+  flex-direction: column;
+`;
+
 const View: React.FC<Props> = () => {
-  return <div>hello</div>;
+    return (
+        <Container>
+          <Heading.Three>Purchase summary</Heading.Three>
+          <Body.Small>Transaction placed on November 4, 2020</Body.Small>
+          <Car>
+              <Picture alt='car' src='/assets/car.png' width="182px" height="104px" />
+            <CarInfo>
+              <Title.Three>2018 Land Rover Range Rover Sport</Title.Three>
+              <Body.Regular>SE</Body.Regular>
+              <Body.Regular>20,818 miles</Body.Regular>
+            </CarInfo>
+          </Car>
+        </Container>
+    );
 };
 
 export default View;
---
+++ apps/checkout/src/stories/pages/congrats/sections/PurchaseSummary.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { withDesign } from 'storybook-addon-designs';
+import { ThemeProvider } from 'styled-components';
+import { getVroomTheme } from 'vroom-ui/src/foundation/themes/Vroom';
+
+import PurchaseSummary from "../../../../modules/congrats/sections/PurchaseSummary";
+
+const theme = getVroomTheme('/assets/fonts/Vroom');
+
+export const Success = (): JSX.Element => {
+    return (
+        <ThemeProvider theme={theme}>
+            <PurchaseSummary/>
+        </ThemeProvider>
+    );
+};
+
+Success.parameters = {
+    design: {
+        type: 'figma',
+        url:
+            'https://www.figma.com/file/rsB4AJNk9uq81u1ia2U0G1/EComm-Port?node-id=263%3A0',
+    },
+};
+
+export default {
+    title: 'Checkout/Congrats/Sections/Purchase Summary',
+    decorators: [withDesign],
+};
