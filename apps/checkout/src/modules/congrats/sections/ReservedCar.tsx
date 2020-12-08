251a1d402ba3e1ad473243688dd28a2f7afa0a17
Claudiu Badau <claudiu.badau@vroom.com>
2020-12-08 11:14:56 -0500

    Update ReservedCar.tsx

--- apps/checkout/src/modules/congrats/sections/ReservedCar.tsx
+++ apps/checkout/src/modules/congrats/sections/ReservedCar.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
-import Icon, { Icons } from 'vroom-ui/src/elements/Icon/Icon';
-import { Picture } from 'vroom-ui/src/elements/Picture';
-import { ThemeProps } from 'vroom-ui/src/foundation/themes/types';
-import { addStyleForMobile } from 'vroom-ui/src/foundation/themes/Vroom';
-import { Body, Heading, Link, Title } from 'vroom-ui/src/foundation/Typography';
+import Icon, {Icons} from 'vroom-ui/src/elements/Icon/Icon';
+import {Picture} from 'vroom-ui/src/elements/Picture';
+import {ThemeProps} from 'vroom-ui/src/foundation/themes/types';
+import {addStyleForMobile} from 'vroom-ui/src/foundation/themes/Vroom';
+import {Body, Heading, Link, Title} from 'vroom-ui/src/foundation/Typography';
 
 const primaryBrand = (props: { theme: ThemeProps }): string =>
-  props.theme.colors.primary.brand;
+    props.theme.colors.primary.brand;
+
+const primaryWhite = (props: { theme: ThemeProps }): string =>
+    props.theme.colors.primary.white;
+
+const grayFour = (props: { theme: ThemeProps }): string =>
+    props.theme.colors.gray.four;
+
+const Background = styled.div`
+  display: flex;
+  background: linear-gradient(${primaryBrand} 70%, ${grayFour} 30%);
+  justify-content: center;
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 48px;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.15);
+  margin: 64px;
+  box-shadow: 0px 4px 24px 4px rgba(0, 0, 0, 0.1);
+  background: ${primaryWhite};
   ${addStyleForMobile(`
         padding: 16px;
    `)}
+`;
+
+const Content = styled.div`
+  display: flex;
+`;
+
+const Information = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 32px;
 `;
 
 const CarPicture = styled.div`
@@ -85,59 +109,65 @@ const Schedule = styled(Link)`
 `;
 
 interface Props {
-  data: {
-    car: string;
-    email: string;
-    phoneNumber: string;
-    image: {
-      alt: string;
-      src: string;
+    data: {
+        car: string;
+        email: string;
+        phoneNumber: string;
+        image: {
+            alt: string;
+            src: string;
+        };
     };
-  };
 }
 
 const ReservedCar: React.FC<Props> = ({
-  data: {
-    car,
-    email,
-    phoneNumber,
-    image: { alt, src },
-  },
-}): JSX.Element => {
-  return (
-    <Container>
-      <CarHeading>your car is reserved!</CarHeading>
-      <CarPicture>
-        <Picture alt={alt} src={src} width="100%" aspectRatio="3:2" />
-      </CarPicture>
-      <CarTitle>{car}</CarTitle>
-      <Steps>
-        <Step>
-          <Check icon={Icons.CHECKMARK_SMALL} />
-          <Body.Regular>
-            The email was sent to <Bold>{email}</Bold>.
-          </Body.Regular>
-        </Step>
-        <Step>
-          <Check icon={Icons.CHECKMARK_SMALL} />
-          <Body.Regular>
-            A Vroom representative will reach out to your phone number{' '}
-            <Bold>+{phoneNumber}</Bold> within the next 24 hours of a business
-            day.{' '}
-          </Body.Regular>
-        </Step>
-        <Step>
-          <Check icon={Icons.CHECKMARK_SMALL} />
-          <Body.Regular>
-            <Schedule href="https://www.vroom.com/schedule" blank>
-              Schedule a time
-            </Schedule>{' '}
-            to talk with the Vroom team.
-          </Body.Regular>
-        </Step>
-      </Steps>
-    </Container>
-  );
+                                          data: {
+                                              car,
+                                              email,
+                                              phoneNumber,
+                                              image: {alt, src},
+                                          },
+                                      }): JSX.Element => {
+    return (
+        <Background>
+            <Container>
+                <CarHeading>your car is reserved!</CarHeading>
+                <Content>
+                    <CarPicture>
+                        <Picture alt={alt} src={src} width="100%" aspectRatio="3:2"/>
+                    </CarPicture>
+                    <Information>
+                        <CarTitle>{car}</CarTitle>
+                        <Steps>
+                            <Step>
+                                <Check icon={Icons.CHECKMARK_SMALL}/>
+                                <Body.Regular>
+                                    The email was sent to <Bold>{email}</Bold>.
+                                </Body.Regular>
+                            </Step>
+                            <Step>
+                                <Check icon={Icons.CHECKMARK_SMALL}/>
+                                <Body.Regular>
+                                    A Vroom representative will reach out to your phone number{' '}
+                                    <Bold>+{phoneNumber}</Bold> within the next 24 hours of a business
+                                    day.{' '}
+                                </Body.Regular>
+                            </Step>
+                            <Step>
+                                <Check icon={Icons.CHECKMARK_SMALL}/>
+                                <Body.Regular>
+                                    <Schedule href="https://www.vroom.com/schedule" blank>
+                                        Schedule a time
+                                    </Schedule>{' '}
+                                    to talk with the Vroom team.
+                                </Body.Regular>
+                            </Step>
+                        </Steps>
+                    </Information>
+                </Content>
+            </Container>
+        </Background>
+    );
 };
 
 export default ReservedCar;
