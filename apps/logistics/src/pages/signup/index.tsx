2a0b170bcea1e1a0c380696f86550b865aa2e8e5
Kevin Parrott <kevin.parrott@vroom.com>
2020-12-16 15:28:24 -0500

    fix for merged Page component

--- apps/logistics/src/pages/signup/index.tsx
+++ apps/logistics/src/pages/signup/index.tsx
@@ -1,14 +1,14 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+import Page from 'src/components/Page';
 import Signup, { SignupContext } from 'src/modules/signup';
 import SignupModel from 'src/modules/signup/Model';
-import Page from 'src/Page';
 
 const SignupPage: NextPage = () => {
   const model = new SignupModel();
   return (
-    <Page name="Sign Up" description="Carrier Portal">
+    <Page name="Sign Up">
       <SignupContext.Provider value={model}>
         <Signup />
       </SignupContext.Provider>
