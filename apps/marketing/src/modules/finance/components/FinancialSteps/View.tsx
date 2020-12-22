1e3771b3b8aa6d0348649d02d2380b5e329a6b32
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2020-12-22 15:24:00 -0500

    Added Financial Steps section

--- apps/marketing/src/modules/finance/components/FinancialSteps/View.tsx
+++ apps/marketing/src/modules/finance/components/FinancialSteps/View.tsx
@@ -15,13 +15,24 @@ const Container = styled('div')(({ theme }) => ({
   alignItems: 'center',
   padding: theme.spacing(9, 6),
   [theme.breakpoints.down('sm')]: {
-    padding: theme.spacing(4, 3),
+    padding: theme.spacing(4, 0),
+  },
+}));
+
+const Content = styled('div')(({ theme }) => ({
+  background: `linear-gradient(-85deg, ${theme.palette.background.paper} 37%, ${theme.palette.background.default} 37%)`,
+  maxWidth: '1280px',
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  [theme.breakpoints.down('sm')]: {
+    padding: theme.spacing(6, 2),
+    background: theme.palette.background.default,
   },
 }));
 
 const Title = styled(Typography)(({ theme }) => ({
   marginBottom: theme.spacing(4),
-  textAlign: 'center',
   letterSpacing: '1px',
   [theme.breakpoints.down('lg')]: {
     fontSize: '42px',
@@ -32,16 +43,74 @@ const Title = styled(Typography)(({ theme }) => ({
     lineHeight: '32px',
   },
   [theme.breakpoints.down('xs')]: {
+    textAlign: 'center',
     fontSize: '28px',
     lineHeight: '32px',
   },
 }));
 
+const Image = styled('img')(({ theme }) => ({
+  width: '50%',
+  height: '50%',
+  [theme.breakpoints.down('sm')]: {
+    display: 'none',
+  },
+}));
+
+const TextSection = styled('div')(({ theme }) => ({
+  width: '50%',
+  margin: theme.spacing(11),
+  [theme.breakpoints.down('sm')]: {
+    width: '60%',
+    margin: 'auto',
+  },
+  [theme.breakpoints.down('xs')]: {
+    width: '100%',
+    margin: 0,
+  },
+}));
+
+const Steps = styled('ol')(({ theme }) => ({
+  paddingLeft: theme.spacing(2),
+}));
+
+const ListItem = styled('li')(({ theme }) => ({
+  marginBottom: theme.spacing(3),
+}));
+
+const StepTitle = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(1),
+  fontSize: '20px',
+  lineHeight: '26px',
+}));
+
+const StepDescription = styled(Typography)(() => ({
+  fontSize: '18px',
+  lineHeight: '25px',
+}));
+
 const FinancialStepsView: React.FC<Props> = ({ viewModel }) => {
-  const { title } = viewModel;
+  const { title, steps, stepsImage } = viewModel;
   return (
     <Container>
-      <Title variant="h2">{title}</Title>
+      <Content>
+        <TextSection>
+          <Title variant="h2">{title}</Title>
+          <Steps>
+            {steps.map((step) => (
+              <ListItem key={step.key}>
+                <StepTitle variant="body1" fontWeight={600}>
+                  {step.title}
+                </StepTitle>
+                <StepDescription variant="body1">
+                  {step.description}
+                </StepDescription>
+              </ListItem>
+            ))}
+          </Steps>
+        </TextSection>
+        <Image src={stepsImage.src} alt={stepsImage.alt} />
+      </Content>
     </Container>
   );
 };
--- apps/marketing/src/modules/finance/components/FinancialSteps/ViewModel.tsx
+++ apps/marketing/src/modules/finance/components/FinancialSteps/ViewModel.tsx
@@ -1,5 +1,31 @@
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
 class FinancialStepsViewModel {
   readonly title = 'How It Works';
+  readonly steps = [
+    {
+      key: 0,
+      title: 'Find Your Ride',
+      description: `Browse thousands of high-quality, low-mileage vehicles, and find the one that's right for you. Shop now`,
+    },
+    {
+      key: 1,
+      title: 'Get Your Financing Terms',
+      description: `Click here to apply in minutes. You will need to select a vehicle first to receive exact financing terms.`,
+    },
+    {
+      key: 2,
+      title: 'Sign, Pay and Drive',
+      description:
+        'Finalize your purchase, then schedule your delivery or in-store pickup. If you finance with us, Vroom will register your vehicle on your behalf and mail your registration and plates to you, once your state DMV has prepared them.',
+    },
+  ];
+  readonly stepsImage = {
+    src: `${publicRuntimeConfig.BASE_PATH}/modules/vroom/images/finance/parallax.png`,
+    alt: 'steps',
+  };
 }
 
 export default FinancialStepsViewModel;
