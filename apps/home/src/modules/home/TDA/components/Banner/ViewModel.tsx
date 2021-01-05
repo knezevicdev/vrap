d3c85d977d430116878646cca39ec4431e64f76d
Dylan Mortimer <dylanmortimer@DylanM-MBP16-DET.lan>
2021-01-05 13:19:16 -0500

    text update

--- apps/home/src/modules/home/TDA/components/Banner/ViewModel.tsx
+++ apps/home/src/modules/home/TDA/components/Banner/ViewModel.tsx
@@ -1,6 +1,6 @@
 class ViewModel {
   readonly bannerText: string =
-    'Our Stafford store is currently open Mon-Sat 10am to 6pm daily. All of our Buying Centers are open in the Houston Metro Area. We can provide an online, convenient sales process and free "at home" delivery service if you live within 40 miles of the Stafford store. We are also offering curb-side pickup. Call us today at 832-225-3686. Terms and conditions apply.';
+    'Our full service Stafford store is currently open Mon-Sat 10am to 6pm daily as are all of our Buying Centers in the Houston Metro Area. We can provide an online, convenient sales process and free "at home" delivery service if you live within 40 miles of the Stafford store. We are also offering curb-side pickup. Call us today on 832-225-3686. Terms and conditions apply.';
   readonly linkText: string = 'SUYC Location Details';
   readonly linkHref: string = '#locations-section';
   scrollToLocation(): void {
