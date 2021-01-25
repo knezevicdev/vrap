ad8b348aadbcc154ca296b99d12ffee5a48b565f
Sankalp Heranjal <sankalpheranjal@SankalpHeranjal-MBP15TB-DET.local>
2021-01-25 14:28:26 -0500

    added register user api

---
+++ apps/checkout/src/modules/registration/api/register.test.ts
@@ -0,0 +1,87 @@
+import Axios from 'axios';
+
+import {
+  ErrorResponse,
+  register,
+  SignupRequest,
+  SuccessResponse,
+} from './register';
+jest.mock('axios');
+const mockedAxios = Axios as jest.Mocked<typeof Axios>;
+
+jest.mock('next/config', () => {
+  return (): unknown => {
+    return {
+      publicRuntimeConfig: {
+        VROOM_URL: 'some-url',
+      },
+    };
+  };
+});
+
+describe('Register User Test', () => {
+  const mockSignupRequest: SignupRequest = {
+    username: 'mock-email',
+    phone: 'mock-number',
+    password: 'mock-password',
+    firstName: 'mock-firstname',
+    middleName: '',
+    lastName: 'mock-lastname',
+    optIn: true,
+    optInSms: true,
+    userAgent: 'mock-user-agent',
+    ipAddress: 'mock-ip',
+  };
+
+  describe('successful registration', () => {
+    const expectedResponse: SuccessResponse = {
+      data: {
+        accountId: 123456,
+        accessToken: 'mock-access-token',
+        refreshToken: 'mock-refresh-token',
+      },
+    };
+
+    beforeEach(() => {
+      mockedAxios.post.mockResolvedValue(expectedResponse);
+    });
+
+    it('should register user', async () => {
+      const actualResponse = await register(mockSignupRequest);
+      expect(actualResponse).toBe(expectedResponse.data);
+      expect(mockedAxios.post).toHaveBeenCalledWith(
+        'some-url/api/auth/signup',
+        mockSignupRequest
+      );
+    });
+  });
+
+  describe('error during registration', () => {
+    const expectedResponse: ErrorResponse = {
+      error: {
+        type: 'mock-type',
+        title: 'mock-title',
+        details: [
+          {
+            message: 'mock-message',
+            meta: {},
+          },
+        ],
+        correlationId: 'mock-correlation-id',
+      },
+    };
+
+    beforeEach(() => {
+      mockedAxios.post.mockRejectedValue(expectedResponse);
+    });
+
+    it('should give an error', async () => {
+      const actualResponse = await register(mockSignupRequest);
+      expect(actualResponse).toBe(expectedResponse.error);
+      expect(mockedAxios.post).toHaveBeenCalledWith(
+        'some-url/api/auth/signup',
+        mockSignupRequest
+      );
+    });
+  });
+});
---
+++ apps/checkout/src/modules/registration/api/register.ts
@@ -0,0 +1,53 @@
+import Axios from 'axios';
+import getConfig from 'next/config';
+
+const {
+  publicRuntimeConfig: { VROOM_URL },
+} = getConfig();
+
+export interface SignupRequest {
+  username: string;
+  phone: string;
+  password: string;
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  optIn: boolean;
+  optInSms: boolean;
+  userAgent: string;
+  ipAddress: string;
+}
+
+interface Data {
+  accountId: number;
+  accessToken: string;
+  refreshToken: string;
+}
+
+interface Details {
+  message: string;
+  meta: unknown;
+}
+
+interface Error {
+  type: string;
+  title: string;
+  details: Details[];
+  correlationId: string;
+}
+
+export interface SuccessResponse {
+  data: Data;
+}
+
+export interface ErrorResponse {
+  error: Error;
+}
+
+export const register = (
+  request: SignupRequest
+): Promise<SuccessResponse | ErrorResponse> => {
+  return Axios.post(`${VROOM_URL}/api/auth/signup`, request)
+    .then((response) => response.data)
+    .catch((error) => error.error);
+};
