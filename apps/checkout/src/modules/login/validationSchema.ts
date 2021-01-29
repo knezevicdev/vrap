0e40e6ec5e6da4c2b8323a330ed41d099ba50795
Pooja Vaidya <poojavaidya@PoojaVaidya-MBPTB15-NYC.local>
2021-01-29 14:21:20 -0500

    update login validation

--- apps/checkout/src/modules/login/validationSchema.ts
+++ apps/checkout/src/modules/login/validationSchema.ts
@@ -2,17 +2,7 @@ import * as Yup from 'yup';
 
 const errorMessages = {
   email: 'Please enter a valid email address',
-  password: 'Needs 8 characters, uppercase, numbers',
-  passwordSize: 'Needs 8 characters',
-  lowercase: 'Needs lowercase',
-  uppercase: 'Needs uppercase',
-  number: 'Needs numbers',
-};
-
-const regex = {
-  lowercase: /^(?=.*[a-z]).+$/,
-  uppercase: /^(?=.*[A-Z]).+$/,
-  numbers: /^(?=.*[0-9]).+$/,
+  password: 'Please enter a valid password'
 };
 
 const emailValidation = Yup.string()
@@ -21,16 +11,7 @@ const emailValidation = Yup.string()
 
 const passwordValidation = Yup.string()
   .required(errorMessages.password)
-  .min(8, errorMessages.passwordSize)
-  .test('has-lowercase', errorMessages.lowercase, (value) =>
-    value ? regex.lowercase.test(value) : false
-  )
-  .test('has-uppercase', errorMessages.uppercase, (value) =>
-    value ? regex.uppercase.test(value) : false
-  )
-  .test('has-numbers', errorMessages.number, (value) =>
-    value ? regex.numbers.test(value) : false
-  );
+  .min(8, errorMessages.password)
 
 export const ValidationSchema = Yup.object({
   username: emailValidation,
