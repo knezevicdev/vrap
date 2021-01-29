f7e924d10a1e3054f3781f1ed616587f18c3855a
Dylan Mortimer <dylanmortimer@DylanM-MBP16-DET.local>
2021-01-29 18:22:04 -0500

    TDA attrib fix, bump home v

--- apps/home/src/modules/home/TDA/components/Hero/BuySellTrade/Sell/LicensePlate/ViewModel.ts
+++ apps/home/src/modules/home/TDA/components/Hero/BuySellTrade/Sell/LicensePlate/ViewModel.ts
@@ -128,7 +128,9 @@ class LicensePlateViewModel {
       const queryString = stringify(this.homeStore.query, {
         addQueryPrefix: true,
       });
-      const vitParams = `vit_source=texasdirectauto&vit_medium=wl&vit_dest=vroom&vit_brand=TDA`;
+      const vitParams = `vit_source=texasdirectauto&vit_medium=wl&vit_dest=vroom&vit_brand=TDA&dealership=${encodeURIComponent(
+        'Texas Direct Auto'
+      )}`;
       window.location.href = `${
         VROOM_URL || ''
       }/sell/vehicleInformation/${vin}${queryString}${
