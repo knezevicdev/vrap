15a2330dc994d33b6d738f85fe5f3eaed3e61e82
Pooja Vaidya <poojavaidya@PoojaVaidya-MBPTB15-NYC.local>
2021-02-05 15:59:20 -0500

    add pending deal dialog

---
+++ apps/checkout/src/stories/pages/PendingDealDialog/PendingDealDialog.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PendingDealDialog from '../../../modules/common/PendingDealDialog';
+
+export const Default = (): JSX.Element => {
+  return <PendingDealDialog />;
+};
+
+export default {
+  title: 'Checkout/PendingDealDialog',
+};
