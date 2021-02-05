f7a9089def55cd8cd523a8b567a145910f0095d4
Pooja Vaidya <poojavaidya@PoojaVaidya-MBPTB15-NYC.local>
2021-02-05 16:32:09 -0500

    add vehicle sold dialog

---
+++ apps/checkout/src/stories/pages/VehicleSoldDialog/VehicleSoldDialog.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import VehicleSoldDialog from '../../../modules/common/VehicleSoldDialog';
+
+export const Default = (): JSX.Element => {
+  return <VehicleSoldDialog carName='2017 Kia Optima' />;
+};
+
+export default {
+  title: 'Checkout/VehicleSoldDialog',
+};
