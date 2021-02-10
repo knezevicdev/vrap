863798ac508146cc1ed6661084899f3fb9082699
Kyle Platt <kyle.platt@vroom.com>
2021-02-10 12:56:31 -0500

    Changing legal text once more

--- apps/product/src/modules/inventory/Santader/components/LegalFooter/ViewModel.ts
+++ apps/product/src/modules/inventory/Santader/components/LegalFooter/ViewModel.ts
@@ -8,7 +8,7 @@ class LegalFooterViewModel {
 
   getLegalText(): string[] {
     return [
-      `Price displayed does not include: Pre-delivery service charges of $285.50  (MA residents only - $300.50) • Delivery charge of $${this.deliveryFee} • FL, NJ or NY residents only - Electronic registration filing charge of $15.00, or applicable taxes, title, tag and registration charges which will be calculated at the time of purchase.`,
+      `Price displayed does not include: Pre-delivery service charges of $285.50  (MA residents only - $300.50) • Delivery charge of $${this.deliveryFee} • FL, NJ or NY residents only - Electronic registration filing charge of $15.00 • Applicable taxes, title, tag and registration charges which will be calculated at the time of purchase.`,
       'Prices are subject to change.',
       'We make every effort to provide accurate vehicle information on this page, but please verify before purchasing.',
     ];
--- apps/product/src/modules/inventory/Vroom/components/LegalFooter/ViewModel.test.ts
+++ apps/product/src/modules/inventory/Vroom/components/LegalFooter/ViewModel.test.ts
@@ -21,7 +21,7 @@ describe('Price ViewModel Tests', () => {
       inventoryStore.deliveryFee = 799;
       const viewModel = new ViewModel(inventoryStore);
       expect(viewModel.getLegalText()).toStrictEqual([
-        'Price displayed does not include: Pre-delivery service charges of $285.50  (MA residents only - $300.50) • Delivery charge of $799 • FL, NJ or NY residents only - Electronic registration filing charge of $15.00, or applicable taxes, title, tag and registration charges which will be calculated at the time of purchase.',
+        'Price displayed does not include: Pre-delivery service charges of $285.50  (MA residents only - $300.50) • Delivery charge of $799 • FL, NJ or NY residents only - Electronic registration filing charge of $15.00 • Applicable taxes, title, tag and registration charges which will be calculated at the time of purchase.',
         'Prices are subject to change.',
         'We make every effort to provide accurate vehicle information on this page, but please verify before purchasing.',
       ]);
