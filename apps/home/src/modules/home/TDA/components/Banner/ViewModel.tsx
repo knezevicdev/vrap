b5c3ae7dcc3dc0a7aca4912ca77162e8076766a6
Dylan Mortimer <dylanmortimer@DylanM-MBP16-DET.local>
2021-02-18 18:35:08 -0500

    open TDA stores

--- apps/home/src/modules/home/TDA/components/Banner/ViewModel.tsx
+++ apps/home/src/modules/home/TDA/components/Banner/ViewModel.tsx
@@ -1,6 +1,6 @@
 class ViewModel {
   readonly bannerText: string =
-    'Due to the inclement weather, all Houston area locations will be closed Wednesday February 17th. You can still reach us via phone at (832) 225-3686.';
+    'Our full service Stafford store is currently open Mon-Sat 10am to 6pm daily as are all of our Buying Centers in the Houston Metro Area. We can provide an online, convenient sales process and free "at home" delivery service if you live within 40 miles of the Stafford store. We are also offering curb-side pickup. Call us today on 832-225-3686. Terms and conditions apply.';
   readonly linkText: string = 'SUYC Location Details';
   readonly linkHref: string = '#locations-section';
   scrollToLocation(): void {
