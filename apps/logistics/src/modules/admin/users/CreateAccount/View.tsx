3528c8d9683d0e730dba5dfc2b6cdee65fbcf6da
Sasha Kondrashov <timofey.kondrashov@vroom.com>
2021-03-09 10:13:52 -0500

    notify admin that an email was sent

--- apps/logistics/src/modules/admin/users/CreateAccount/View.tsx
+++ apps/logistics/src/modules/admin/users/CreateAccount/View.tsx
@@ -5,9 +5,11 @@ import {
   Icon,
   InputAdornment,
   Paper,
+  Snackbar,
   TextField,
   Typography,
 } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 import { observer } from 'mobx-react';
 import React from 'react';
 
@@ -23,49 +25,64 @@ const CreateAccountView: React.FC<Props> = ({ viewModel }) => {
   };
 
   return (
-    <Paper square>
-      <Box p={2}>
-        <form onSubmit={handleSubmit}>
-          <Grid container alignItems="center" spacing={2}>
-            <Grid item xs={12}>
-              <label htmlFor="create-account-email">
-                <Typography>
-                  To invite a user, enter their email in the box below and click
-                  send
-                </Typography>
-              </label>
-              <TextField
-                id="create-account-email"
-                value={viewModel.email}
-                onChange={(e): void => {
-                  viewModel.email = e.target.value;
-                }}
-                placeholder="Email"
-                variant="outlined"
-                required
-                fullWidth
-                autoComplete="email"
-                type="email"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Button
-                        type="submit"
-                        color="primary"
-                        endIcon={<Icon>send</Icon>}
-                        disabled={viewModel.disabled}
-                      >
-                        Send
-                      </Button>
-                    </InputAdornment>
-                  ),
-                }}
-              />
+    <>
+      <Paper square>
+        <Box p={2}>
+          <form onSubmit={handleSubmit}>
+            <Grid container alignItems="center" spacing={2}>
+              <Grid item xs={12}>
+                <label htmlFor="create-account-email">
+                  <Typography>
+                    To invite a user, enter their email in the box below and
+                    click send
+                  </Typography>
+                </label>
+                <TextField
+                  id="create-account-email"
+                  value={viewModel.email}
+                  onChange={(e): void => {
+                    viewModel.email = e.target.value;
+                  }}
+                  placeholder="Email"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  autoComplete="email"
+                  type="email"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <Button
+                          type="submit"
+                          color="primary"
+                          endIcon={<Icon>send</Icon>}
+                          disabled={viewModel.disabled}
+                        >
+                          Send
+                        </Button>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </Box>
-    </Paper>
+          </form>
+        </Box>
+      </Paper>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={viewModel.snackbar}
+        autoHideDuration={6000}
+        onClose={viewModel.resetSnackbar}
+      >
+        <Alert severity="success" onClose={viewModel.resetSnackbar}>
+          Approval email successfully sent to new user
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
