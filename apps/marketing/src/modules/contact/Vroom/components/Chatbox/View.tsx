0214a0f4d06519520ff4392f396a165b7f63957b
Joseph Cho <jscho13@gmail.com>
2021-03-18 23:14:09 -0400

    add segment events

---
+++ apps/marketing/src/modules/contact/Vroom/components/Chatbox/View.tsx
@@ -0,0 +1,145 @@
+import getConfig from 'next/config';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  viewModel: ViewModel;
+}
+
+const { publicRuntimeConfig } = getConfig();
+
+const Chatbox: React.FC = ({ viewModel }) => {
+  const [booted, setBooted] = useState(false);
+  const [showChatIcon, setShowChatIcon] = useState(true);
+  const chatIcon = `${publicRuntimeConfig.BASE_PATH}/modules/vroom/icons/chat-icon.svg`;
+  const router = useRouter();
+  const { chatboxOpen } = router.query || false;
+
+  const initChat = () => {
+    /* TODO:
+      This implementation is pretty horrible. @ts-ignore is a major red flag.
+      Pypestream is brought in from `webchat-public.js` in useEffect, but when we compile Next.js isn't gonna know what it is.
+      I've tried copying the file to `/public` dir but pypestream complains about a VPN error.
+      The pypestream implemention uses old school `document.addEventListener` but there are implementaiton issues.
+      React will lose track of the event listener, not clean up properly, and you'll need to hack your way with refs.
+      Even then results are not guaranteed and may not even be a better implementation */
+
+    const dev = publicRuntimeConfig.NODE_ENV !== 'production';
+    const pypeStreamConfig = dev
+      ? {
+          domain: 'dev',
+          env: 'sandbox',
+          beta: true,
+          /* eslint-disable @typescript-eslint/camelcase */
+          gtm_id: 'GTM-PZJGZ67',
+        }
+      : {
+          domain: 'prod',
+          env: 'prod',
+          beta: true,
+          /* eslint-disable @typescript-eslint/camelcase */
+          gtm_id: 'GTM-PZJGZ67',
+        };
+
+    // @ts-ignore
+    Pypestream('config', pypeStreamConfig);
+
+    const chatContainer = document.getElementById('chat-container');
+    // @ts-ignore
+    Pypestream(
+      'boot',
+      { APP_ID: '70c71811-1c35-4db7-b9d2-21754f24ba0c' },
+      chatContainer
+    );
+
+    viewModel.trackChatboxOpened();
+    setBooted(true);
+    setShowChatIcon(false);
+
+    // @ts-ignore
+    Pypestream('onShow', function () {
+      setShowChatIcon(false);
+    });
+
+    // @ts-ignore
+    Pypestream('onHide', function () {
+      setShowChatIcon(true);
+    });
+
+    // @ts-ignore
+    Pypestream('onChatEnd', function () {
+      viewModel.trackChatboxClosed();
+    });
+  };
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://webchat-sandbox.pypestream.com/webchat-public.js';
+    document.body.appendChild(script);
+
+    // https://tdalabs.atlassian.net/browse/CW-82
+    const timer = setTimeout(() => {
+      if (chatboxOpen) initChat();
+    }, 1000);
+
+    return (): void => {
+      document.body.removeChild(script);
+      clearTimeout(timer);
+    };
+  }, [chatboxOpen]);
+
+  const handleOnClick = (): void => {
+    if (!booted) {
+      initChat();
+    } else {
+      // @ts-ignore
+      Pypestream('toggle');
+      setShowChatIcon(!showChatIcon);
+    }
+  };
+
+  return (
+    <>
+      {showChatIcon && (
+        <ChatIconContainer id="toggle-chat" onClick={handleOnClick}>
+          <ChatboxIcon src={chatIcon} />
+        </ChatIconContainer>
+      )}
+      <ChatContainer id="chat-container"></ChatContainer>
+    </>
+  );
+};
+
+const ChatIconContainer = styled.div`
+  align-items: center;
+  background-color: white;
+  border-radius: 50%;
+  bottom: 10px;
+  box-shadow: 0 1px 5px 0 rgb(0 0 0 / 50%);
+  display: flex;
+  height: 70px;
+  justify-content: center;
+  margin: 10px;
+  position: absolute;
+  right: 70px;
+  width: 70px;
+  z-index: 10;
+
+  @media (max-width: 1920px) {
+    right: 10px;
+  }
+`;
+
+const ChatboxIcon = styled.img`
+  height: 40px;
+  width: 40px;
+`;
+
+const ChatContainer = styled.div`
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+`;
+
+export default Chatbox;
