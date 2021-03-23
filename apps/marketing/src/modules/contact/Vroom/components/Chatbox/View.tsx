549277ad88e3d92b2237ef2e90b039baca11d57f
Joseph Cho <jscho13@gmail.com>
2021-03-23 11:05:33 -0400

    run linter

--- apps/marketing/src/modules/contact/Vroom/components/Chatbox/View.tsx
+++ apps/marketing/src/modules/contact/Vroom/components/Chatbox/View.tsx
@@ -29,7 +29,9 @@ const View: React.FC<Props> = ({ viewModel }) => {
 
     const chatContainer = document.getElementById('chat-container');
     const dev = publicRuntimeConfig.NODE_ENV !== 'production';
-    const appId = dev ? '70c71811-1c35-4db7-b9d2-21754f24ba0c' : 'e0987638-07c6-43b4-a10e-fa51ad9174d2';
+    const appId = dev
+      ? '70c71811-1c35-4db7-b9d2-21754f24ba0c'
+      : 'e0987638-07c6-43b4-a10e-fa51ad9174d2';
     const pypeStreamConfig = dev
       ? {
           domain: 'dev',
@@ -50,11 +52,7 @@ const View: React.FC<Props> = ({ viewModel }) => {
     Pypestream('config', pypeStreamConfig);
 
     // @ts-ignore
-    Pypestream(
-      'boot',
-      { APP_ID: appId },
-      chatContainer
-    );
+    Pypestream('boot', { APP_ID: appId }, chatContainer);
 
     // @ts-ignore
     Pypestream('onShow', function () {
