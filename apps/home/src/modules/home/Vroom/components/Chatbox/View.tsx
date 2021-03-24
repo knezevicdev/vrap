83d9c3a214a05fdfa5f25e9058b7720a66ece99b
Joseph Cho <jscho13@gmail.com>
2021-03-24 13:39:06 -0400

    pass node_env

--- apps/home/src/modules/home/Vroom/components/Chatbox/View.tsx
+++ apps/home/src/modules/home/Vroom/components/Chatbox/View.tsx
@@ -17,6 +17,7 @@ const View: React.FC<Props> = ({ viewModel }) => {
   const chatIcon = `${publicRuntimeConfig.BASE_PATH}/modules/home/images/chat-icon.svg`;
   const router = useRouter();
   const { chatboxOpen } = router.query || false;
+  const dev = publicRuntimeConfig.NODE_ENV !== 'production';
 
   const initChat = () => {
     /* TODO:
@@ -28,6 +29,9 @@ const View: React.FC<Props> = ({ viewModel }) => {
       Even then results are not guaranteed. */
 
     const chatContainer = document.getElementById('chat-container');
+    const appId = dev
+      ? '70c71811-1c35-4db7-b9d2-21754f24ba0c'
+      : 'e0987638-07c6-43b4-a10e-fa51ad9174d2';
     const pypeStreamConfig = dev
       ? {
           domain: 'dev',
@@ -48,7 +52,7 @@ const View: React.FC<Props> = ({ viewModel }) => {
     Pypestream('config', pypeStreamConfig);
 
     // @ts-ignore
-    Pypestream('boot', { APP_ID: publicRuntimeConfig.PYPESTREAM_APP_ID }, chatContainer);
+    Pypestream('boot', { APP_ID: appId }, chatContainer);
 
     // @ts-ignore
     Pypestream('onShow', function () {
@@ -72,7 +76,11 @@ const View: React.FC<Props> = ({ viewModel }) => {
 
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://webchat-sandbox.pypestream.com/webchat-public.js';
+    if (dev) {
+      script.src = 'https://webchat-sandbox.pypestream.com/webchat-public.js';
+    } else {
+      script.src = 'https://webchat.pypestream.com/webchat-public.js';
+    }
     document.body.appendChild(script);
 
     // https://tdalabs.atlassian.net/browse/CW-82
