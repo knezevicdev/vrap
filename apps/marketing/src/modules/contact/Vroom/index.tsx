fabe31e5785bdf83b85ae23a086c23bf320bad6d
Dylan Mortimer <dylan.mortimer@vroom.com>
2021-04-02 12:42:53 -0400

    embed iframe for vroom contact page

--- apps/marketing/src/modules/contact/Vroom/index.tsx
+++ apps/marketing/src/modules/contact/Vroom/index.tsx
@@ -1,21 +1,15 @@
-import { StandardFooter } from '@vroom-web/footer-components';
-import { SimpleHeader } from '@vroom-web/header-components';
-import getConfig from 'next/config';
 import React from 'react';
 
-import ContactLabel from './components/ContactLabel';
-
-const { publicRuntimeConfig } = getConfig();
-
 const Vroom: React.FC = () => {
-  const gearboxPrivateUrl = publicRuntimeConfig.GEARBOX_PRIVATE_URL;
-
   return (
-    <>
-      <SimpleHeader gearboxPrivateUrl={gearboxPrivateUrl} />
-      <ContactLabel />
-      <StandardFooter />
-    </>
+      <iframe
+        src={'https://vroom-web.force.com/support/s/welcome'}
+        style={{
+          width: '100vw',
+          height: '100vh',
+        }}
+        sandbox="allow-same-origin allow-scripts"
+      />
   );
 };
 
