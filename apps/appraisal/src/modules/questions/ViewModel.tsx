95bf0649b4eadf48a54bd6824d6784bba55a1c45
Kevin Parrott <kevin.parrott@vroom.com>
2021-04-08 11:28:44 -0400

    replace footer and questions section links

--- apps/appraisal/src/modules/questions/ViewModel.tsx
+++ apps/appraisal/src/modules/questions/ViewModel.tsx
@@ -4,7 +4,7 @@ class ViewModel {
   readonly sendMessage: string = 'SEND A MESSAGE';
   readonly phoneNumber: string = '(855) 524-1300';
 
-  readonly faqLink: string = 'https://vroom.zendesk.com/hc/en-us';
+  readonly faqLink: string = '/contact';
   readonly emailLink: string = '/contact';
   readonly phoneLink: string = `tel:1-${this.phoneNumber.split(' ').join('-')}`;
 }
