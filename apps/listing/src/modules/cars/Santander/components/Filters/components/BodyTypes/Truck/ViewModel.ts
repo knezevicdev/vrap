bdf702f9d734b0af0fb4b7155301aff9eaa87214
Tim Laster <timothy.laster@vroom.com>
2021-04-15 14:42:23 -0400

    retire product and listing

--- apps/listing/src/modules/cars/Santander/components/Filters/components/BodyTypes/Truck/ViewModel.ts
+++
@@ -1,7 +0,0 @@
-class TruckViewModel {
-  readonly cabTypeFilterLabel: string = 'Cabin Size';
-  readonly bedLengthFilterLabel: string = 'Bed Length';
-  readonly towingCapacityFilterLabel: string = 'Towing Capacity';
-}
-
-export default TruckViewModel;
--- apps/product/src/modules/inventory/Santader/components/SalesContact/ViewModel.ts
+++
@@ -1,6 +0,0 @@
-class SalesContactViewModel {
-  readonly title: string = 'Questions?';
-  readonly body: string = 'Call us at (855) 659-0278';
-}
-
-export default SalesContactViewModel;
