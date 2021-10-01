48945fe68f4c12b3e106cb354e68a504d5a6ba5a
Jacob Lee <jacoblee@NYCLTC02FQ5G8MD6M.local>
2021-10-01 11:06:45 -0400

    change test

--- src/modules/verification/__test__/ReviewViewModel.test.ts
+++ src/modules/verification/__test__/ReviewViewModel.test.ts
@@ -1,6 +1,7 @@
-import AnalyticsHandler from '../../../integrations/AnalyticsHandler';
 import store from '../../../store';
 import ViewModel from '../review/ViewModel';
+
+import AnalyticsHandler from 'src/integrations/AnalyticsHandler';
 
 jest.mock('src/networking/request');
 
@@ -8,15 +9,7 @@ describe('Review component test', () => {
   const stores = new store();
   let viewModel: ViewModel;
   const analyticHandler = new AnalyticsHandler();
-  const pageViewd = jest.spyOn(
-    analyticHandler,
-    'trackVerificationReviewViewed'
-  );
 
-  const verificationSubmitted = jest.spyOn(
-    analyticHandler,
-    'trackVerificationSubmitted'
-  );
   beforeEach(() => {
     viewModel = new ViewModel(stores);
   });
@@ -33,16 +26,23 @@ describe('Review component test', () => {
   });
 
   it('tracker should called when pageLoad called', () => {
+    const pageViewd = jest.spyOn(
+      analyticHandler,
+      'trackVerificationReviewViewed'
+    );
     viewModel.onPageLoad();
     expect(pageViewd).toHaveBeenCalled();
   });
 
   it('test when get verification data ', () => {
     viewModel.getVerificationDetails('cb5b06d43cb95286ceeb50efc7a82e08');
-    console.log('owners info <<<<>>>>>>> ', stores.verification.ownerInfo);
   });
 
   it('test when verification submitted ', () => {
+    const verificationSubmitted = jest.spyOn(
+      analyticHandler,
+      'trackVerificationSubmitted'
+    );
     viewModel.verificationSubmit();
     expect(verificationSubmitted).toHaveBeenCalled();
   });
