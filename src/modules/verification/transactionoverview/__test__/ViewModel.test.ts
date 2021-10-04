2eec96da11e45b2d34fa2b21ecfd2156c43104ee
Jacob Lee <jacoblee@NYCLTC02FQ5G8MD6M.local>
2021-10-04 17:40:31 -0400

    add api transction test

--- src/modules/verification/transactionoverview/__test__/ViewModel.test.ts
+++ src/modules/verification/transactionoverview/__test__/ViewModel.test.ts
@@ -1,5 +1,7 @@
 import ViewModel from '../ViewModel';
 
+import { getOfferDetails } from 'src/networking/__mocks__/request';
+import * as Request from 'src/networking/request';
 import store from 'src/store';
 
 jest.mock('src/networking/request');
@@ -12,6 +14,8 @@ jest.mock('next/config', () => (): unknown => ({
 describe('Review component test', () => {
   const stores = new store();
   let viewModel: ViewModel;
+  const spyRequest = jest.spyOn(Request, 'getOfferDetails');
+  spyRequest.mockResolvedValue(getOfferDetails());
 
   beforeEach(() => {
     viewModel = new ViewModel(stores);
@@ -21,18 +25,19 @@ describe('Review component test', () => {
     expect(viewModel.title).toEqual('transaction summary');
   });
 
-  // it('test api call getOfferDetail', () => {
-  //   viewModel.getOfferDetail('26300');
-  //   const response = {
-  //     make: 'NISSAN',
-  //     model: 'Murano',
-  //     price: 10854,
-  //     trim: 'Utility 4D SV 2WD V6',
-  //     year: 2016,
-  //     miles: 99999,
-  //   };
-  //   expect(JSON.stringify(stores.offer.getOfferDetail)).toEqual(
-  //     JSON.stringify(response)
-  //   );
-  // });
+  it('test api call getOfferDetail', async () => {
+    await viewModel.getOfferDetail('26300');
+    const response = {
+      make: 'NISSAN',
+      model: 'Murano',
+      price: 10854,
+      trim: 'Utility 4D SV 2WD V6',
+      year: 2016,
+      miles: 999999,
+    };
+
+    expect(JSON.stringify(stores.offer.offerDetail)).toEqual(
+      JSON.stringify(response)
+    );
+  });
 });
