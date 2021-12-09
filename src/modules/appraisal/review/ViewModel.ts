ae96bb08e512410065e1bdf90b377b00a2406725
Dušan Aleksić <dusan.aleksic@vroom.com>
2021-12-09 15:49:11 +0100

    Fix appraisal posting for appraisal review

--- src/modules/appraisal/review/ViewModel.ts
+++ src/modules/appraisal/review/ViewModel.ts
@@ -3,14 +3,14 @@ import { isErrorResponse } from '@vroom-web/networking';
 import { makeRequestBody } from '../utils';
 
 import { AppraisalRespData } from 'src/networking/models/Appraisal';
-import { postAppraisal } from 'src/networking/request';
+import { postAppraisalReview } from 'src/networking/request';
 
 export default class AppraisalReviewModel {
   readonly title: string = 'my appraisal review';
 
   async submitAppraisal(data: any): Promise<void> {
     try {
-      await postAppraisal(makeRequestBody(data)).then((resp) => {
+      await postAppraisalReview(makeRequestBody(data)).then((resp) => {
         if (isErrorResponse(resp)) throw resp;
         const returnData: AppraisalRespData = resp.data;
         localStorage.removeItem('appraisal');
