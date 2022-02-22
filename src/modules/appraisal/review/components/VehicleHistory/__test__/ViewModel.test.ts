90be7b4ab70e270427965cfbe6f4f1ae34ae596e
Jacob Lee <jacoblee@NYCLTC02FQ5G8MD6M.local>
2022-02-22 14:01:50 -0500

    add unit test vehicle history

--- src/modules/appraisal/review/components/VehicleHistory/__test__/ViewModel.test.ts
+++ src/modules/appraisal/review/components/VehicleHistory/__test__/ViewModel.test.ts
@@ -1,3 +1,5 @@
+import { NextRouter } from 'next/router';
+
 import ViewModel from '../ViewModel';
 
 import store from 'src/store';
@@ -11,8 +13,12 @@ describe('Owner Infomation Review component test', () => {
   const stores = new store();
   let viewModel: ViewModel;
 
+  const router = ({
+    push: jest.fn(),
+  } as unknown) as NextRouter;
+
   beforeEach(() => {
-    viewModel = new ViewModel(stores);
+    viewModel = new ViewModel(stores, router);
   });
 
   it('test readonly initial values', () => {
@@ -20,5 +26,10 @@ describe('Owner Infomation Review component test', () => {
     expect(viewModel.accident).toEqual('Accident');
     expect(viewModel.title).toEqual('Title');
     expect(viewModel.edit).toEqual('Edit');
+  });
+
+  it('test handleEditClick, should router called ', () => {
+    viewModel.handleEditClick();
+    expect(router.push).toHaveBeenCalled();
   });
 });
