ddd463a1a1765ce2739407e52f5839d9847dfb73
Dušan Aleksić <dusan.aleksic@vroom.com>
2022-03-28 14:40:04 +0200

    Remove core/input replace input used in base formik class with vroom-ui version

--- src/core/FormikInput.tsx
+++ src/core/FormikInput.tsx
@@ -1,7 +1,7 @@
+import { Input } from '@vroom-web/ui-lib';
 import { Field } from 'formik';
 import React from 'react';
-
-import { CoreInput, CoreInputProps } from './CoreInput';
+import styled from 'styled-components';
 
 interface FormikProps {
   field: any;
@@ -11,17 +11,55 @@ interface FormikProps {
   };
 }
 
+interface CoreInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  error?: string;
+  label: string;
+  placeholder: string;
+  value?: string;
+  touched?: boolean;
+  fluid?: boolean;
+  appendComponent?: React.FC | null; //Allow to inject components below the text field
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  options?: string[];
+}
+
 const FormikInput: React.FC<CoreInputProps> = (props) => {
+  const {
+    placeholder,
+    name,
+    error,
+    label,
+    className,
+    value,
+    disabled,
+    type,
+    fluid,
+    appendComponent,
+    ...rest
+  } = props;
+
+  const isSuccess = !!value && value.length > 0 && !!error && error.length > 0;
+
   return (
-    <Field name={props.name}>
+    <Field name={name}>
       {({ field, meta }: FormikProps): JSX.Element => {
         return (
-          <CoreInput
-            {...props}
-            {...field}
-            error={meta.error}
-            touched={meta.touched}
-          />
+          <Container className={className} fluid={fluid}>
+            <Input
+              {...rest}
+              label={label}
+              value={value}
+              error={meta.error}
+              placeholder={placeholder}
+              name={name}
+              id={name}
+              disabled={disabled}
+              type={type}
+              success={isSuccess}
+            />
+            {appendComponent && appendComponent}
+          </Container>
         );
       }}
     </Field>
@@ -29,3 +67,16 @@ const FormikInput: React.FC<CoreInputProps> = (props) => {
 };
 
 export default FormikInput;
+
+interface ContainerProps {
+  fluid?: boolean;
+}
+
+const Container = styled.div<ContainerProps>`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  position: relative;
+  margin-bottom: 20px;
+  width: ${({ fluid }): string => (fluid ? '100%' : 'min-content')};
+`;
