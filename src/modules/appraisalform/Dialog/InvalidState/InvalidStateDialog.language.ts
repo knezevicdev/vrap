fb3ca090b008afd67c5a1b03801ec6cfcf34d122
nemanjakazic <nemanja.kazic@vast.com>
2022-07-22 16:46:24 +0200

    [AC-2288] Change text of the Invalid state modal CTA and lead to inventory when clicked

--- src/modules/appraisalform/Dialog/InvalidState/InvalidStateDialog.language.ts
+++ src/modules/appraisalform/Dialog/InvalidState/InvalidStateDialog.language.ts
@@ -2,5 +2,5 @@ export const lang = {
   title: 'Location not supported',
   desc: (state: string): string =>
     `We are not purchasing vehicles from ${state} at this time, and are unable to provide a price for you.`,
-  updateState: 'Update Zip Code',
+  browseInventory: 'Browse Inventory',
 };
