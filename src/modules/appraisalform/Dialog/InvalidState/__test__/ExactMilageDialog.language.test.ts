566d8eacfddf421103a3cf5f30385e43885295e0
nemanjakazic <nemanja.kazic@vast.com>
2022-08-03 18:21:00 +0200

    [AC-2288] Text correction

--- src/modules/appraisalform/Dialog/InvalidState/__test__/ExactMilageDialog.language.test.ts
+++ src/modules/appraisalform/Dialog/InvalidState/__test__/ExactMilageDialog.language.test.ts
@@ -4,7 +4,7 @@ describe('exact milage dialog language test', () => {
   test('test const', () => {
     expect(lang.title).toEqual('Location not supported');
     expect(lang.desc('Arizona')).toEqual(
-      'We are not purchasing vehicles from Arizona at this time, and are unable to provide a price for you.'
+      'Sorry, we are not purchasing vehicles from Arizona at this time, and are unable to provide a price for you.'
     );
     expect(lang.browseInventory).toEqual('Browse Inventory');
   });
