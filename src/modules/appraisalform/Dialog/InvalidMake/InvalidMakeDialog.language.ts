261befd9a327a715abaa7a6f0876b3483e5df3c2
Nikola Knežević <knezevicdev@gmail.com>
2022-11-09 23:53:05 +0100

    [AC-2424] Expand makes blocklist

--- src/modules/appraisalform/Dialog/InvalidMake/InvalidMakeDialog.language.ts
+++
@@ -1,5 +0,0 @@
-export const lang = {
-  title: 'Make not supported',
-  desc: `Sorry, we are not purchasing Maserati vehicles at this time, and are unable to provide a price for you.`,
-  browseInventory: 'Browse Inventory',
-};
--- src/modules/appraisalform/Dialog/InvalidMake/index.tsx
+++ src/modules/appraisalform/Dialog/InvalidMake/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import { observer } from 'mobx-react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
-import View from './View';
+import {
+  BrowseInventoryCTA,
+  Container,
+  Line,
+  Modal,
+  ModalContent,
+  ModalDesc,
+  ModalImage,
+  ModalTitle,
+} from './Style.css';
 
-const Dialog: React.FC = () => {
-  return <View />;
+interface Props {
+  make: string;
+}
+
+const DialogView: React.FC<Props> = ({ make }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  });
+
+  const goToInventory = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    router.push({ pathname: '/cars' });
+  };
+
+  return (
+    <Container>
+      <Modal>
+        <ModalContent>
+          <ModalTitle>
+            <div>Make not supported</div>
+          </ModalTitle>
+          <Line />
+          <ModalImage />
+          <ModalDesc>
+            Sorry, we are not purchasing {make} vehicles at this time, and are
+            unable to provide a price for you.
+          </ModalDesc>
+          <BrowseInventoryCTA onClick={goToInventory}>
+            Browse Inventory
+          </BrowseInventoryCTA>
+        </ModalContent>
+      </Modal>
+    </Container>
+  );
 };
 
-export default Dialog;
+export default observer(DialogView);
