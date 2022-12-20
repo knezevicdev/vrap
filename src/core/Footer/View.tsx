f5906a3841a0da51732b5160adf469e67d23f42d
Alan Alickovic <alan.alickovic@vroom.com>
2022-12-20 16:25:44 +0000

    update footer

--- src/core/Footer/View.tsx
+++ src/core/Footer/View.tsx
@@ -1,103 +1,19 @@
-import { Icon } from '@vroom-web/ui-lib';
+import { VroomFooter } from '@vroom-web/shared-components';
 import React from 'react';
 import styled from 'styled-components';
 
-import { Body } from '../../core/Typography';
-import { DesktopLinks } from './Desktop/Links';
-import { MobileLinks } from './Mobile/Links';
-import FooterViewModel from './ViewModel';
-
-import { Icons } from 'src/core/Icon';
-
 const Container = styled.div<{ hasOverlay: boolean }>`
-  display: flex;
-  background: #041022;
-  padding: 48px 64px 32px 64px;
-  margin-top: auto;
   ${(props): any => (props.hasOverlay === true ? 'padding-bottom: 180px' : '')};
-  @media (max-width: 599px) {
-    flex-direction: column;
-    padding: 24px;
-    ${(props): any =>
-      props.hasOverlay === true ? 'padding-bottom: 180px' : ''};
-  }
-`;
-
-const Information = styled.div`
-  display: flex;
-  flex-direction: column;
-  @media (max-width: 599px) {
-    order: 2;
-  }
-`;
-
-const Apps = styled.div`
-  display: flex;
-`;
-
-const Socials = styled.div`
-  display: flex;
-  margin: 16px 0;
-`;
-
-const IconSpaced = styled(Icon)`
-  margin-right: 16px;
-`;
-
-const Message = styled(Body.Small)`
-  color: #ffffff;
-  font-weight: 600;
-  margin-top: 16px;
-  margin-bottom: 8px;
-`;
-
-const Copyright = styled(Body.Fine)`
-  color: #ffffff;
-`;
-
-const CustomA = styled.a`
-  line-height: 0;
 `;
 
 interface Props {
-  viewModel: FooterViewModel;
   hasOverlay: boolean;
 }
 
-const FooterView: React.FC<Props> = ({ viewModel, hasOverlay }) => {
-  const { facebook, twitter, instagram } = viewModel.socialLinks;
-  const { apple, google } = viewModel.appLinks;
-
+const FooterView: React.FC<Props> = ({ hasOverlay }) => {
   return (
     <Container hasOverlay={hasOverlay}>
-      <Information>
-        <CustomA href={viewModel.vroomLink}>
-          <Icon icon={Icons.VROOM_WHITE} />
-        </CustomA>
-        <Message>{viewModel.appMessage}</Message>
-        <Apps>
-          <CustomA href={google}>
-            <IconSpaced icon={Icons.GOOGLE_PLAY} />
-          </CustomA>
-          <CustomA href={apple}>
-            <Icon icon={Icons.APPLE_STORE} />
-          </CustomA>
-        </Apps>
-        <Socials>
-          <CustomA href={facebook}>
-            <IconSpaced icon={Icons.FACEBOOK} />
-          </CustomA>
-          <CustomA href={twitter}>
-            <IconSpaced icon={Icons.TWITTER} />
-          </CustomA>
-          <CustomA href={instagram}>
-            <Icon icon={Icons.INSTAGRAM} />
-          </CustomA>
-        </Socials>
-        <Copyright>{viewModel.copyrightMessage}</Copyright>
-      </Information>
-      <DesktopLinks sections={viewModel.sections} />
-      <MobileLinks sections={viewModel.sections} />
+      <VroomFooter />
     </Container>
   );
 };
