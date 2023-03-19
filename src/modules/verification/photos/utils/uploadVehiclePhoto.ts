87b732754ed67b853786e64f10f3b4217a6f1536
Nikola Knežević <nikola.knezevic@vast.com>
2023-03-19 14:59:45 +0100

    [AC-2441] Upload vehicle photos as whole

--- src/modules/verification/photos/utils/uploadVehiclePhoto.ts
+++ src/modules/verification/photos/utils/uploadVehiclePhoto.ts
@@ -10,40 +10,24 @@ export enum DocumentFileType {
   DRIVER_SIDE_FRONT_SEAT = 'driverSideFrontSeat',
 }
 
-function generateUniqueId(priceId: string) {
-  const timestamp = new Date().getTime();
-  return `${timestamp}-${priceId}`;
-}
-
 export const uploadVehiclePhoto = async (
   file: ActualFileObject,
   fileType: DocumentFileType,
   vin: string,
   priceId: string
 ): Promise<boolean> => {
-  const uploadId = generateUniqueId(priceId);
-  const chunkSize = 1024 * 100; // 100 KB
+  const fileBuffer = await file.arrayBuffer();
 
-  const totalChunks = Math.ceil(file.size / chunkSize);
-
-  for (let chunkIndex = 0; chunkIndex < totalChunks; chunkIndex++) {
-    const start = chunkIndex * chunkSize;
-    const end = Math.min(start + chunkSize, file.size);
-    const chunk = file.slice(start, end);
-
-    const formData = new FormData();
-    formData.append('chunk', chunk);
-
-    await axios.post(
-      `/appraisal/api/photos/${vin}/upload?priceId=${priceId}&fileType=${fileType}&uploadId=${uploadId}&chunkIndex=${chunkIndex}&totalChunks=${totalChunks}`,
-      chunk,
-      {
-        headers: {
-          'Content-Type': 'application/octet-stream',
-        },
-      }
-    );
-  }
+  await axios.post(
+    `/appraisal/api/photos/${vin}/upload?priceId=${priceId}&fileType=${fileType}`,
+    fileBuffer,
+    {
+      headers: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/octet-stream',
+      },
+    }
+  );
 
   return true;
 };
