aae2ec2f6be8077172aa11b6b85c7d9c4874c77a
Nikola Knežević <nikola.knezevic@vast.com>
2023-04-03 10:32:32 +0200

    [AC-2441] Use canvas to remove image metadata

--- src/modules/verification/photos/utils/removeFileMetadataAndGenerateBuffer.ts
+++ src/modules/verification/photos/utils/removeFileMetadataAndGenerateBuffer.ts
@@ -1,48 +1,45 @@
 import { ActualFileObject } from 'filepond';
 
-/**
- * Removes EXIF metadata from an image file and returns its contents as an ArrayBuffer.
- *
- * EXIF metadata can include a variety of fields such as camera settings, location
- * data, and image editing software information. Some applications, such as AWS WAF, may reject image files
- * that contain certain types of EXIF metadata.
- */
 const removeFileMetadataAndGenerateBuffer = async (
   file: ActualFileObject
 ): Promise<ArrayBuffer> => {
-  const arrayBuffer = await file.arrayBuffer();
-  const dataView = new DataView(arrayBuffer);
+  let canvas: HTMLCanvasElement | undefined = undefined;
+  let cleanObjectUrl: () => void = () => {
+    // nothing
+  };
+  try {
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(file);
+    cleanObjectUrl = () => URL.revokeObjectURL(img.src);
+    await new Promise((resolve, reject) => {
+      img.onload = resolve;
+      img.onerror = reject;
+    });
 
-  let offset = 0;
-  while (offset < dataView.byteLength) {
-    const marker = dataView.getUint16(offset, false);
-    offset += 2;
+    canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext('2d');
+    if (!ctx) throw new Error('Could not get canvas context');
+    ctx.drawImage(img, 0, 0);
 
-    if (marker === 0xffe1) {
-      const length = dataView.getUint16(offset, false);
-      offset += 2;
-
-      if (offset + length > dataView.byteLength) {
-        break;
+    const newBlob = (await new Promise((resolve) => {
+      if (!canvas) {
+        resolve(null);
+        return;
       }
+      canvas.toBlob(resolve, file.type);
+    })) as Blob | null;
+    if (!newBlob) throw new Error('Could not generate new blob');
 
-      if (
-        String.fromCharCode(dataView.getUint8(offset)) === 'E' &&
-        String.fromCharCode(dataView.getUint8(offset + 1)) === 'x' &&
-        String.fromCharCode(dataView.getUint8(offset + 2)) === 'if' &&
-        String.fromCharCode(dataView.getUint8(offset + 3)) === '\0'
-      ) {
-        return new ArrayBuffer(offset + 2 + length - 10);
-      }
-
-      offset += length - 2;
-    } else {
-      const length = dataView.getUint16(offset, false);
-      offset += length - 2;
-    }
+    return newBlob.arrayBuffer();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    cleanObjectUrl();
+    canvas?.remove();
   }
-
-  return arrayBuffer;
 };
 
 export default removeFileMetadataAndGenerateBuffer;
