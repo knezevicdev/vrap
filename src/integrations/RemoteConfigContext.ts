6149b877ae530afbd452960a4ea427973e434bc3
andy.lu <andy.lu@vroom.com>
2023-04-04 11:00:45 -0400

    updating header and remove firebase remote config

--- src/integrations/RemoteConfigContext.ts
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app';
-import { createContext } from 'react';
-
-export const RemoteConfigContext =
-  createContext<firebase.remoteConfig.RemoteConfig>(
-    {} as firebase.remoteConfig.RemoteConfig
-  );
