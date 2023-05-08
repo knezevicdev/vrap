51c67a27128a017514d754bc3b91c897bda4017a
Nikola Knežević <nikola.knezevic@vast.com>
2023-05-09 00:38:27 +0200

    [AC-2605] Update pickup address confirmation strategy

--- src/modules/verification/unified/steps/PickupInformation/steps/PickupAddressConfirmation/PickupAddressConfirmation.tsx
+++ src/modules/verification/unified/steps/PickupInformation/steps/PickupAddressConfirmation/PickupAddressConfirmation.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { shallow } from 'zustand/shallow';
 
 import { Col, Row } from '../../../../../../../styled/grid';
 import SelectBoxes from '../../../../components/SelectBoxes';
 import { WizardStepProps } from '../../../../components/WizardForm';
+import useVerificationStore from '../../../../store/store';
+import { Field, FullInfo, SectionTitle } from '../../../../Styled.css';
 
 const PickupAddressConfirmation = ({
   form,
@@ -11,29 +14,52 @@ const PickupAddressConfirmation = ({
 }: WizardStepProps<{
   pickupAddressConfirmation: string;
 }>) => {
+  const { pickupAddress, pickupState, pickupZip, pickupCity, pickupApt } =
+    useVerificationStore(
+      (state) => ({
+        pickupAddress: state.pickupAddress,
+        pickupState: state.pickupState,
+        pickupZip: state.pickupZip,
+        pickupCity: state.pickupCity,
+        pickupApt: state.pickupApt,
+      }),
+      shallow
+    );
+
   return (
-    <Row>
-      <Col
-        size={{
-          default: 1 / 2,
-          mobile: 1,
-        }}
-      >
-        <SelectBoxes
-          label="Is the pick up address the same as the primary owner's address?"
-          options={['Yes', 'No']}
-          control={form.control}
-          id={'pickupAddressConfirmation'}
-          onChange={(value) => {
-            if (value === 'Yes') {
-              goToStep?.(3);
-              return;
-            }
-            nextStep?.();
+    <div>
+      <SectionTitle>Pick up address</SectionTitle>
+      <Row>
+        <FullInfo>
+          <Field>
+            {`${pickupApt} ${pickupAddress} ${pickupCity}, ${pickupState} ${pickupZip}`.trim()}
+          </Field>
+        </FullInfo>
+      </Row>
+      <br />
+      <Row>
+        <Col
+          size={{
+            default: 1 / 2,
+            mobile: 1,
           }}
-        />
-      </Col>
-    </Row>
+        >
+          <SelectBoxes
+            label="Is the pickup address correct?"
+            options={['Yes', 'No']}
+            control={form.control}
+            id={'pickupAddressConfirmation'}
+            onChange={(value) => {
+              if (value === 'Yes') {
+                goToStep?.(3);
+                return;
+              }
+              nextStep?.();
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
   );
 };
 
